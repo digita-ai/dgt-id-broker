b81642ae5d6e9381199bdb35daa6d103cc972f7b
Wouter Termont <woutermont@gmail.com>
2021-04-10 20:51:45 +0200

    build: test-commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,13 @@
-module.exports = {extends: ['@commitlint/config-lerna-scopes']};
+// NOTE: this can also be in 'commitlint' object of package.json, e.g. extending from private package
+
+const {utils: {getPackages}} = require('@commitlint/config-lerna-scopes');
+
+module.exports = {
+    extends: [
+        '@commitlint/config-conventional',
+        '@commitlint/config-lerna-scopes'
+    ],
+    rules: {
+        'scope-enum': async ctx => [2, 'always', [...(await getPackages(ctx)), 'all', 'multi']]
+    }
+}
