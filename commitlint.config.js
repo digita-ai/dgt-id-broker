2770e054f0ba0d8a1e489a1e4de8f58c4dfdde52
Wouter Termont <woutermont@gmail.com>
2021-04-10 21:03:45 +0200

    build(all): test-pushhook

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,9 +5,10 @@ const {utils: {getPackages}} = require('@commitlint/config-lerna-scopes');
 module.exports = {
     extends: [
         '@commitlint/config-conventional',
-        '@commitlint/config-lerna-scopes'
+        '@commitlint/config-lerna-scopes',
     ],
     rules: {
-        'scope-enum': async ctx => [2, 'always', [...(await getPackages(ctx)), 'all', 'multi']]
+        'scope-empty': [2, 'never'],
+        'scope-enum': async ctx => [2, 'always', [...(await getPackages(ctx)), 'all', 'multi']],
     }
 }
