2f173cffceae08794be31a11dbfc779f1e77e42e
Abel <abelfrans@gmail.com>
2021-04-21 10:09:31 +0200

    chore: refactor DpopTokenRequestHandler

--- packages/dgt-id-proxy/lib/server/dpop-token-request.handler.ts
+++ packages/dgt-id-proxy/lib/server/dpop-token-request.handler.ts
@@ -1,8 +1,8 @@
 import { join } from 'path';
 import { readFile } from 'fs/promises';
-import { HttpHandler, HttpHandlerContext, HttpHandlerResponse } from '@digita-ai/handlersjs-http';
-import { of, from, throwError, combineLatest } from 'rxjs';
-import { switchMap, map, catchError } from 'rxjs/operators';
+import { HttpHandler, HttpHandlerContext, HttpHandlerResponse, MethodNotAllowedHttpError } from '@digita-ai/handlersjs-http';
+import { of, from, throwError, combineLatest, EMPTY, Observable } from 'rxjs';
+import { switchMap, map, catchError, withLatestFrom, tap } from 'rxjs/operators';
 import { EmbeddedJWK } from 'jose/jwk/embedded';
 import { calculateThumbprint } from 'jose/jwk/thumbprint';
 import { jwtVerify } from 'jose/jwt/verify';
@@ -25,14 +25,13 @@ export class DpopTokenRequestHandler extends HttpHandler {
     private handler: HttpHandler,
     private keyValueStore: InMemoryStore<string, string[]>,
     private pathToJwks: string,
+    private proxyUrl: string,
   ) {
     super();
 
     if(!handler){
       throw new Error('A HttpHandler must be provided');
     }
-
-    keyValueStore.set('jtis', []);
   }
 
   /**
@@ -69,112 +68,86 @@ export class DpopTokenRequestHandler extends HttpHandler {
       }
 
       const dpopJWT = context.request.headers.dpop;
-      let jwk: JWK;
-      return combineLatest(
-        from(
-          jwtVerify(
-            dpopJWT,
-            EmbeddedJWK,
-            {
-              maxTokenAge: `60 seconds`,
-              typ: 'dpop+jwt',
-              algorithms: [
-                'RS256',
-                'PS256',
-                'ES256',
-                'EdDSA',
-              ],
-            },
-          ),
+
+      const decodedDpopJwt = from(
+        jwtVerify(
+          dpopJWT,
+          EmbeddedJWK,
+          {
+            maxTokenAge: `60 seconds`,
+            typ: 'dpop+jwt',
+            algorithms: [
+              'RS256',
+              'PS256',
+              'ES256',
+              'EdDSA',
+            ],
+          },
         ),
-        from(this.keyValueStore.get('jtis')),
-      ).pipe(
-        switchMap(([ { payload, protectedHeader }, jtis ]) => {
-          jwk = protectedHeader.jwk as JWK;
+      );
 
-          if (!payload.jti || typeof payload.jti !== 'string') {
-            throw new Error(JSON.stringify({ error: 'invalid_dpop_proof', error_description: 'must have a jti string property' }));
-          }
-
-          if (jtis?.includes(payload.jti)){
-            throw new Error(JSON.stringify({ error: 'invalid_dpop_proof', error_description: 'jti must be unique' }));
-          }
-
-          jtis?.push(payload.jti);
-          if (jtis) {
-            this.keyValueStore.set('jtis', jtis);
-          }
-
-          if (payload.htm !== context.request.method) {
-            throw new Error(JSON.stringify({ error: 'invalid_dpop_proof', error_description: 'htm does not match the request method' }));
-          }
-
-          // TO DO: FIX HARDCODED URL
-          if (payload.htu !== 'http://localhost:3003/token') {
-            throw new Error(JSON.stringify({ error: 'invalid_dpop_proof', error_description: 'htu does not match' }));
-          }
-
-          delete context.request.headers.dpop;
-          return this.handler.handle(context);
-        }),
-        switchMap((response) => {
-          const parsedBody = JSON.parse(response.body);
-          if (parsedBody.error) {
-            throw new Error(response.body);
-          }
-          return combineLatest(
-            of(response),
-            from(calculateThumbprint(jwk)),
-            from(readFile(join(process.cwd(), this.pathToJwks))),
-          );
-        }),
-        switchMap(([ response, thumbprint, file ]) => {
-          const jwks = JSON.parse((file.toString()));
-
-          return combineLatest(
-            of(response),
-            of(jwks.keys[0]),
-            from(parseJwk(jwks.keys[0])),
-            of(thumbprint),
-          );
-        }),
-        switchMap(([ response, serverJwk, serverJwkAsKeyLike, thumbprint ]) => {
-          const parsedBody = JSON.parse(response.body);
-          const accessTokenPayload = parsedBody.access_token.split('.')[1];
-          const decodedAccessTokenPayload = JSON.parse(decode(accessTokenPayload).toString());
-          decodedAccessTokenPayload.cnf = { 'jkt': thumbprint };
-
-          return combineLatest(
-            of(response),
-            from(new SignJWT(decodedAccessTokenPayload as JWTPayload)
-              .setProtectedHeader({ alg: serverJwk.alg, typ: 'at+jwt', kid: serverJwk.kid })
-              // TO DO: Fix hardcoded URL
-              .setIssuer('http://localhost:3003')
-              .sign(serverJwkAsKeyLike)),
-          );
-        }),
-        switchMap(([ response, dpopBoundAccessToken ]) => {
-          const parsedBody = JSON.parse(response.body);
-          parsedBody.token_type = 'DPoP';
-          parsedBody.access_token = dpopBoundAccessToken;
-          return of({
-            body: JSON.stringify(parsedBody),
-            headers: {
-              'access-control-allow-origin': context.request.headers.origin,
-              'access-control-allow-headers': 'dpop',
-              'Content-Type':  'application/json',
-            },
-            status: 200,
-          });
-        }),
-        catchError((error) => of ({
-          body: error.message,
+      const responseAfterVerification = decodedDpopJwt.pipe(
+        switchMap(({ payload }) => this.verifyDpopProof(context, payload)),
+        catchError((error) => throwError({
+          body: JSON.stringify({ error: 'invalid_dpop_proof', error_description: error.message }),
           headers: { 'access-control-allow-origin': context.request.headers.origin },
           status: 400,
         })),
+        switchMap(() => {
+          // if the observable completes, remove the dpop header from the request and pass it on to the handler
+          delete context.request.headers.dpop;
+          return this.handler.handle(context);
+        }),
+        // if the observable was rejected during the verification process create an error response.
+        switchMap((response) => response.status === 200 ? of(response) : throwError(response)),
       );
-    } else {
+
+      const clientJwk = decodedDpopJwt.pipe(map(({ protectedHeader }) => protectedHeader.jwk as JWK));
+      const jwkFile = from(readFile(join(process.cwd(), this.pathToJwks)));
+
+      const jwksAndThumbprint = combineLatest(jwkFile, clientJwk).pipe(
+        switchMap(([ file, jwk ]) => {
+          const jwks = JSON.parse((file.toString()));
+
+          const rawJwk = of(jwks.keys[0]);
+          const parsedJwk = from(parseJwk(jwks.keys[0]));
+
+          return combineLatest(
+            rawJwk,
+            parsedJwk,
+            from(calculateThumbprint(jwk)),
+          );
+        }),
+      );
+
+      return combineLatest(jwksAndThumbprint, responseAfterVerification).pipe(
+        switchMap(([ [ serverJwk, serverJwkAsKeyLike, thumbprint ], response ]) => {
+          const dpopAccessTokenPayload = this.createDpopAccessTokenPayload(response.body, thumbprint);
+
+          const dpopBoundAccessToken = from(new SignJWT(dpopAccessTokenPayload)
+            .setProtectedHeader({ alg: serverJwk.alg, typ: 'at+jwt', kid: serverJwk.kid })
+            .setIssuer(this.proxyUrl)
+            .sign(serverJwkAsKeyLike));
+
+          return combineLatest(
+            of(response),
+            dpopBoundAccessToken,
+          );
+        }),
+        switchMap(([ response, dpopBoundAccessToken ]) => this.createDpopResponse(
+          response,
+          dpopBoundAccessToken,
+          context.request.headers.origin,
+        )),
+        catchError((error) =>
+          // TODO: Fix this. This is probably not the right way to go about it. This catches the throwError(response) on line 102
+          // but we aren't sure that it's actually of the type HttpHandlerResponse
+          error.body && error.headers && error.status ? of(error) : throwError(error)),
+      );
+    } else if (context.request.method === 'OPTIONS') {
       return this.handler.handle(context);
+    } else {
+      return throwError(new MethodNotAllowedHttpError('this method is not supported.'));
     }
   }
 
@@ -189,5 +162,60 @@ export class DpopTokenRequestHandler extends HttpHandler {
       && context.request.url
       ? of(true)
       : of(false);
+  }
+
+  private verifyDpopProof(context: HttpHandlerContext, payload: JWTPayload): Observable<unknown> {
+
+    // check the parameters, and throw an error if they are missing
+    if (!payload.jti || typeof payload.jti !== 'string') {
+      return throwError(new Error('must have a jti string property'));
+    }
+
+    if (payload.htm !== context.request.method) {
+      return throwError(new Error('htm does not match the request method'));
+    }
+
+    if (payload.htu !== this.proxyUrl + '/token') {
+      return throwError(new Error('htu does not match'));
+    }
+
+    const jti = payload.jti;
+
+    return from(this.keyValueStore.get('jtis')).pipe(
+      switchMap((jtis) => {
+        if (jtis?.includes(jti)) {
+          return throwError(new Error('jti must be unique'));
+        }
+        this.keyValueStore.set('jtis', jtis ? [ ...jtis, jti ] : [ jti ]);
+        return of({});
+      }),
+    );
+  }
+
+  private createDpopAccessTokenPayload(responseBody: string, clientPublicKeyThumbprint: string): JWTPayload {
+    const parsedBody = JSON.parse(responseBody);
+    // split the access token into header, payload, and footer parts, then get the payload
+    const accessTokenPayload = parsedBody.access_token.split('.')[1];
+    // base64url decode the access token payload
+    const decodedAccessTokenPayload = JSON.parse(decode(accessTokenPayload).toString());
+    // set the cnf claim
+    decodedAccessTokenPayload.cnf = { 'jkt': clientPublicKeyThumbprint };
+
+    return decodedAccessTokenPayload;
+  }
+
+  private createDpopResponse(response: HttpHandlerResponse, dpopBoundAccessToken: string, corsOrigin: string) {
+    const parsedBody = JSON.parse(response.body);
+    parsedBody.token_type = 'DPoP';
+    parsedBody.access_token = dpopBoundAccessToken;
+    return of({
+      body: JSON.stringify(parsedBody),
+      headers: {
+        'access-control-allow-origin': corsOrigin,
+        'access-control-allow-headers': 'dpop',
+        'Content-Type':  'application/json',
+      },
+      status: 200,
+    });
   }
 }
