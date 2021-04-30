edc548d8b6f8b2a6550b9353ceb0c0305e4b2a72
Abel <abelfrans@gmail.com>
2021-04-30 11:58:10 +0200

    chore: merge develop into feat/support-dpop

--- packages/dgt-id-proxy/lib/public-api.ts
+++ packages/dgt-id-proxy/lib/public-api.ts
@@ -3,4 +3,5 @@ export * from './server/dpop-token-request.handler';
 export * from './server/jwk-request.handler';
 export * from './storage/in-memory-store';
 export * from './storage/key-value-store';
-
+export * from './server/access-token-decode.handler';
+export * from './server/access-token-encode.handler';
