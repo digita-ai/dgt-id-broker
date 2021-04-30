73baef1388319d4da5bdb2404f53991d7a293a6e
Abel <abelfrans@gmail.com>
2021-04-30 13:16:02 +0200

    chore: added comments in jwk handler

--- packages/dgt-id-proxy/lib/server/jwk-request.handler.ts
+++ packages/dgt-id-proxy/lib/server/jwk-request.handler.ts
@@ -27,17 +27,18 @@ export class JwkRequestHandler extends HttpHandler {
         switchMap((jwks) => {
           const jwksForResponse = {
             keys: jwks.keys.map((jwk: JWK) => ({
-              kty: jwk.kty,
-              use: jwk.use,
+              kty: jwk.kty, // key type: defines thecryptographic algorithm family used with the key
+              use: jwk.use, // The "use" parameter is employed to indicate whether a public key is used for encrypting data or verifying the signature on data.
+              // The "key_ops" (key operations) parameter identifies the operation(s) for which the key is intended to be used
               key_ops: jwk.key_ops ? [ ...jwk.key_ops ] : undefined,
-              kid: jwk.kid,
-              alg: jwk.alg,
-              crv: jwk.crv,
-              e: jwk.e,
-              n: jwk.n,
-              x: jwk.x,
-              x5c: jwk.x5c ? [ ...jwk.x5c ] : undefined,
-              y: jwk.y,
+              kid: jwk.kid, // The "kid" (key ID) parameter is used to match a specific key.
+              alg: jwk.alg, // The "key_ops" (key operations) parameter identifies the operation(s) for which the key is intended to be used
+              crv: jwk.crv, // if key type is elliptical curve, defines the specific curve (ES256, p-256, RS256, etc.)
+              e: jwk.e, // used by RSA key types
+              n: jwk.n, // used by RSA key types
+              x: jwk.x, // used by EC key types
+              x5c: jwk.x5c ? [ ...jwk.x5c ] : undefined, // optional parameter. Read more: https://tools.ietf.org/html/rfc7517#section-4.7
+              y: jwk.y, // used by EC key types
             })),
           };
           return of({
