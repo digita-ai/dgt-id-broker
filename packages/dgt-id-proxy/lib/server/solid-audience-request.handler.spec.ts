0cd2423d7c22e885555633adefab791189e11aad
Abel <abelfrans@gmail.com>
2021-05-03 15:02:48 +0200

    chore: aud handler compatible w/ encoder/decoder

--- packages/dgt-id-proxy/lib/server/solid-audience-request.handler.spec.ts
+++ packages/dgt-id-proxy/lib/server/solid-audience-request.handler.spec.ts
@@ -1,222 +1,107 @@
-import { readFile } from 'fs/promises';
-import { HttpHandler, HttpHandlerContext } from '@digita-ai/handlersjs-http';
+import { HttpHandlerResponse } from '@digita-ai/handlersjs-http';
 import { of } from 'rxjs';
-import { generateKeyPair } from 'jose/util/generate_key_pair';
-import { SignJWT } from 'jose/jwt/sign';
 import { decode } from 'jose/util/base64url';
 import { SolidAudienceRequestHandler } from './solid-audience-request.handler';
 
-jest.mock('fs/promises', () => {
-  const testJwks = {
-    'keys': [
-      {
-        'crv': 'P-256',
-        'x': 'ZXD5luOOClkYI-WieNfw7WGISxIPjH_PWrtvDZRZsf0',
-        'y': 'vshKz414TtqkkM7gNXKqawrszn44OTSR_j-JxP-BlWo',
-        'd': '07JS0yPt-fDABw_28JdENtlF0PTNMchYmfSXz7pRhVw',
-        'kty': 'EC',
-        'kid': 'Eqa03FG9Z7AUQx5iRvpwwnkjAdy-PwmUYKLQFIgSY5E',
-        'alg': 'ES256',
-        'use': 'sig',
-      },
-    ],
-  };
-  return {
-    readFile: jest.fn().mockResolvedValue(Buffer.from(JSON.stringify(testJwks))),
-  };
-});
-
 describe('SolidAudienceRequestHandler', () => {
   let handler: SolidAudienceRequestHandler;
-  let nestedHandler: HttpHandler;
-  let context: HttpHandlerContext;
-
-  const mockedAccessToken = async (aud) => {
-    const keyPair = await generateKeyPair('ES256');
-
-    return new SignJWT(
-      {
-        jti: '49c1b4e1-3747-413b-932f-25668f934fb9',
-        sub: '23121d3c-84df-44ac-b458-3d63a9a05497',
-        scope: '',
-        client_id: 'client',
-        iss: 'http://localhost:3000',
-        aud,
-      },
-    )
-      .setProtectedHeader({ alg: 'ES256', kid: 'keyid', typ: 'at+jwt'  })
-      .setIssuedAt()
-      .setExpirationTime(7200)
-      .sign(keyPair.privateKey);
-  };
+  let response: HttpHandlerResponse;
 
   beforeEach(() => {
-    nestedHandler = {
-      handle: jest.fn(),
-      canHandle: jest.fn(),
-      safeHandle: jest.fn(),
+    handler = new SolidAudienceRequestHandler();
+    response = {
+      body: {
+        access_token: {
+          header: {},
+          payload: {},
+        },
+        expires_in: 7200,
+        scope: '',
+        token_type: 'Bearer',
+      },
+      headers: {},
+      status: 200,
     };
-    handler = new SolidAudienceRequestHandler(nestedHandler, 'assets/jwks.json', 'http://localhost:3003');
-    context = { request: { headers: { 'origin': 'http://localhost' }, method: 'POST', url: new URL('http://digita.ai/') } };
   });
 
   it('should be correctly instantiated', () => {
     expect(handler).toBeTruthy();
   });
 
-  it('should error when no handler or proxyUrl is provided in the constructor', () => {
-    expect(() => new SolidAudienceRequestHandler(undefined, 'assets/jwks.json', 'http://localhost:3003')).toThrow('A handler must be provided');
-    expect(() => new SolidAudienceRequestHandler(null, 'assets/jwks.json', 'http://localhost:3003')).toThrow('A handler must be provided');
-    expect(() => new SolidAudienceRequestHandler(nestedHandler, undefined, 'http://localhost:3003')).toThrow('A pathToJwks must be provided');
-    expect(() => new SolidAudienceRequestHandler(nestedHandler, null, 'http://localhost:3003')).toThrow('A pathToJwks must be provided');
-    expect(() => new SolidAudienceRequestHandler(nestedHandler, 'assets/jwks.json', undefined)).toThrow('A proxyUrl must be provided');
-    expect(() => new SolidAudienceRequestHandler(nestedHandler, 'assets/jwks.json', null)).toThrow('A proxyUrl must be provided');
-
-  });
-
   describe('handle', () => {
-    it('should error when no context was provided', async () => {
-      await expect(() => handler.handle(undefined).toPromise()).rejects.toThrow('Context cannot be null or undefined');
-      await expect(() => handler.handle(null).toPromise()).rejects.toThrow('Context cannot be null or undefined');
+    it('should error when no response is provided', async () => {
+      await expect(() => handler.handle(undefined).toPromise()).rejects.toThrow('response cannot be null or undefined');
+      await expect(() => handler.handle(null).toPromise()).rejects.toThrow('response cannot be null or undefined');
     });
 
-    it('should error when no context request is provided', async () => {
-      context.request = null;
-      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No request was included in the context');
-      context.request = undefined;
-      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No request was included in the context');
+    it('should return the response unedited if the response has a status other than 200', async () => {
+      response.status = 400;
+
+      await expect(handler.handle(response).toPromise()).resolves.toEqual(response);
     });
 
-    it('should error when no context request method is provided', async () => {
-      context.request.method = null;
-      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No method was included in the request');
-      context.request.method = undefined;
-      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No method was included in the request');
+    it('should error when response body does not have an access token or is not in JSON format', async () => {
+      response.body = 'mockbody';
+      await expect(() => handler.handle(response).toPromise()).rejects.toThrow('Response body must contain an access token with a payload in JSON format');
+
+      response.body = {
+        access_token: 'mockToken',
+      };
+      await expect(() => handler.handle(response).toPromise()).rejects.toThrow('Response body must contain an access token with a payload in JSON format');
     });
 
-    it('should error when no context request headers are provided', async () => {
-      context.request.headers = null;
-      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No headers were included in the request');
-      context.request.headers = undefined;
-      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No headers were included in the request');
-    });
-
-    it('should error when no context request url is provided', async () => {
-      context.request.url = null;
-      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No url was included in the request');
-      context.request.url = undefined;
-      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No url was included in the request');
-    });
-
-    it('should error when method is not OPTIONS or POST', async () => {
-      context.request.method = 'GET';
-      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('this method is not supported.');
-    });
-
-    it('should return the response of the nestedHandler unedited when method is OPTIONS', async () => {
-      nestedHandler.handle = jest.fn().mockReturnValueOnce(of({ body: 'options', headers: {}, status: 200 }));
-      context.request.method = 'OPTIONS';
-      await expect(handler.handle(context).toPromise()).resolves.toEqual({ body: 'options', headers: {}, status: 200 });
-    });
-
-    it('should return an error response when the upstream server returns a response with status other than 200', async () => {
-      nestedHandler.handle = jest.fn().mockReturnValueOnce(of({
-        body: JSON.stringify({ error: 'invalid_request', error_description: 'grant request invalid' }),
-        headers: {},
-        status: 400,
-      }));
-
-      await expect(handler.handle(context).toPromise()).resolves.toEqual({
-        body: JSON.stringify({ error: 'invalid_request', error_description: 'grant request invalid' }),
-        headers: {},
-        status: 400,
-      });
-    });
-
-    it('should return a token with the issuer set to the proxy and an aud array containing solid', async () => {
-      // mocked response with audience as a string (single item)
-      const token1 = await mockedAccessToken('client');
-      nestedHandler.handle = jest.fn().mockReturnValueOnce(of({
-        body: JSON.stringify({
-          access_token: token1,
-          expires_in: 7200,
-          scope: '',
-          token_type: 'Bearer',
-        }),
-        headers: {},
-        status: 200,
-      }));
-      const resp1 = await handler.handle(context).toPromise();
+    it('should return a token with an aud array containing solid', async () => {
+      // single aud claim
+      response.body.access_token.payload.aud = 'client';
+      const resp1 = await handler.handle(response).toPromise();
       expect(resp1.status).toEqual(200);
+      expect(resp1.body.access_token.payload).toBeDefined();
+      expect(resp1.body.access_token.payload.aud).toEqual([ 'client', 'solid' ]);
 
-      const parsedBody1 = JSON.parse(resp1.body);
-      expect(parsedBody1.access_token).toBeDefined();
-
-      const tokenPayload1 = JSON.parse(decode(parsedBody1.access_token.split('.')[1]).toString());
-      expect(tokenPayload1.iss).toEqual('http://localhost:3003');
-      expect(tokenPayload1.aud).toEqual([ 'client', 'solid' ]);
-
-      // mocked response with audience as an array
-      const token2 = await mockedAccessToken([ 'client', 'audience' ]);
-      nestedHandler.handle = jest.fn().mockReturnValueOnce(of({
-        body: JSON.stringify({
-          access_token: token2,
-          expires_in: 7200,
-          scope: '',
-          token_type: 'Bearer',
-        }),
-        headers: {},
-        status: 200,
-      }));
-      const resp2 = await handler.handle(context).toPromise();
+      // aud claim as array
+      response.body.access_token.payload.aud = [ 'client', 'audience' ];
+      const resp2 = await handler.handle(response).toPromise();
       expect(resp2.status).toEqual(200);
+      expect(resp2.body.access_token.payload).toBeDefined();
+      expect(resp2.body.access_token.payload.aud).toEqual([ 'client', 'audience', 'solid' ]);
+    });
 
-      const parsedBody2 = JSON.parse(resp2.body);
-      expect(parsedBody2.access_token).toBeDefined();
+    it('should return the token as is if audience is already "solid" or an array containing "solid"', async () => {
+      // aud claim is solid
+      response.body.access_token.payload.aud = 'solid';
+      const resp1 = await handler.handle(response).toPromise();
+      expect(resp1.status).toEqual(200);
+      expect(resp1.body.access_token.payload).toBeDefined();
+      expect(resp1.body.access_token.payload.aud).toEqual('solid');
 
-      const tokenPayload2 = JSON.parse(decode(parsedBody2.access_token.split('.')[1]).toString());
-      expect(tokenPayload2.iss).toEqual('http://localhost:3003');
-      expect(tokenPayload2.aud).toEqual([ 'client', 'audience', 'solid' ]);
+      // aud claim is an array containing solid
+      response.body.access_token.payload.aud = [ 'client', 'solid' ];
+      const resp2 = await handler.handle(response).toPromise();
+      expect(resp2.status).toEqual(200);
+      expect(resp2.body.access_token.payload).toBeDefined();
+      expect(resp2.body.access_token.payload.aud).toEqual([ 'client', 'solid' ]);
     });
 
   });
 
   describe('canHandle', () => {
-    it('should return false if no context was provided', async () => {
+    it('should return false if no response was provided', async () => {
       await expect(handler.canHandle(undefined).toPromise()).resolves.toEqual(false);
       await expect(handler.canHandle(null).toPromise()).resolves.toEqual(false);
     });
 
-    it('should return false if context was provided', async () => {
-      context.request = undefined;
-      await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
-      context.request = null;
-      await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
+    it('should return false if a response was provided without an access_token or payload', async () => {
+      response.body = 'mockbody';
+      await expect(handler.canHandle(response).toPromise()).resolves.toEqual(false);
+
+      response.body = {
+        access_token: 'mockToken',
+      };
+      await expect(handler.canHandle(response).toPromise()).resolves.toEqual(false);
     });
 
-    it('should return false when no context request method is provided', async () => {
-      context.request.method = null;
-      await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
-      context.request.method = undefined;
-      await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
-    });
-
-    it('should return false when no context request headers are provided', async () => {
-      context.request.headers = null;
-      await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
-      context.request.headers = undefined;
-      await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
-    });
-
-    it('should return false when no context request url is provided', async () => {
-      context.request.url = null;
-      await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
-      context.request.url = undefined;
-      await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
-    });
-
-    it('should return true if correct context was provided', async () => {
-      await expect(handler.canHandle(context).toPromise()).resolves.toEqual(true);
+    it('should return true if a correct response was provided', async () => {
+      await expect(handler.canHandle(response).toPromise()).resolves.toEqual(true);
     });
   });
 });
--- packages/dgt-id-proxy/lib/server/solid-audience-request.handler.ts
+++ packages/dgt-id-proxy/lib/server/solid-audience-request.handler.ts
@@ -1,154 +1,73 @@
-import { join } from 'path';
-import { readFile } from 'fs/promises';
-import { HttpHandler, HttpHandlerContext, HttpHandlerResponse, MethodNotAllowedHttpError } from '@digita-ai/handlersjs-http';
-import { Observable, of, from, throwError, zip } from 'rxjs';
-import { switchMap, catchError } from 'rxjs/operators';
-import { decode } from 'jose/util/base64url';
-import { JWK, JWTPayload } from 'jose/webcrypto/types';
-import { SignJWT } from 'jose/jwt/sign';
-import { parseJwk } from 'jose/jwk/parse';
+import { HttpHandlerResponse } from '@digita-ai/handlersjs-http';
+import { Observable, of, throwError } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Handler } from '@digita-ai/handlersjs-core';
 
 /**
- * A {HttpHandler} that adds the 'solid' claim to the aud claim of a JWT Access Token
+ * A {Handler} that adds 'solid' to the audience claim of a JWT Access Token
  */
-export class SolidAudienceRequestHandler extends HttpHandler {
-/**
- * Creates a {SolidAudienceRequestHandler} passing requests through the given handler.
- *
- * @param {HttpHandler} handler - the handler through which to pass incoming requests.
- * @param {string} pathToJwks - the path to a json file containing jwks.
- * @param {string} proxyUrl - the url of the upstream server.
- */
-  constructor(private handler: HttpHandler, private pathToJwks: string, private proxyUrl: string) {
-    super();
-
-    if (!handler) {
-      throw new Error('A handler must be provided');
-    }
-
-    if(!pathToJwks){
-      throw new Error('A pathToJwks must be provided');
-    }
-
-    if(!proxyUrl){
-      throw new Error('A proxyUrl must be provided');
-    }
-  }
+export class SolidAudienceRequestHandler extends Handler<HttpHandlerResponse, HttpHandlerResponse> {
 
   /**
-   * Passes the context's incoming request to the nested handler.
-   * It then handles the response. If the response is a 200 response it adds
-   * the string 'solid' to the aud claim of the Access Token.
-   * Otherwise it returns an error response.
+   * Handles the response. If the response is a 200 response it adds
+   * the string 'solid' to the audience claim of the Access Token.
    *
-   * @param {HttpHandlerContext} context
+   * @param {HttpHandlerResponse} response
    */
-  handle(context: HttpHandlerContext) {
-    if (!context) {
-      return throwError(new Error('Context cannot be null or undefined'));
+  handle(response: HttpHandlerResponse) {
+    if (!response) {
+      return throwError(new Error('response cannot be null or undefined'));
     }
 
-    if (!context.request) {
-      return throwError(new Error('No request was included in the context'));
+    if (response.status !== 200) {
+      return of(response);
     }
 
-    if (!context.request.method) {
-      return throwError(new Error('No method was included in the request'));
+    if (!response.body.access_token || !response.body.access_token.payload){
+      return throwError(new Error('Response body must contain an access token with a payload in JSON format'));
     }
 
-    if (!context.request.headers) {
-      return throwError(new Error('No headers were included in the request'));
-    }
-
-    if (!context.request.url) {
-      return throwError(new Error('No url was included in the request'));
-    }
-
-    if (context.request.method === 'OPTIONS') {
-      return this.handler.handle(context);
-    }
-
-    if (context.request.method !== 'POST') {
-      return throwError(new MethodNotAllowedHttpError('this method is not supported.'));
-    }
-
-    return this.getUpstreamResponse(context).pipe(
-      // creates a claim extended token
-      switchMap((response) => zip(of(response), this.createClaimExtendedToken(response.body))),
+    // creates a claim extended token
+    return this.addAudienceClaimToToken(response.body.access_token).pipe(
       // creates a response including the new access token
-      switchMap(([ response, token ]) => this.createAccessTokenResponse(
-        response,
-        token,
-        context.request.headers.origin,
-      )),
-      // switches any errors with body into responses; all the rest are server errors which will hopefully be caught higher
-      catchError((error) => error.body && error.headers && error.status ? of(error) : throwError(error)),
+      switchMap((token) => this.createAccessTokenResponse(response, token)),
     );
   }
 
-  private getUpstreamResponse = (context: HttpHandlerContext) => this.handler.handle(context).pipe(
-    switchMap((response) => response.status === 200 ? of(response) : throwError(response)),
-  );
-
-  private getSigningKit = () => from(readFile(join(process.cwd(), this.pathToJwks))).pipe(
-    switchMap<Buffer, JWK>((keyFile) => of(JSON.parse(keyFile.toString()).keys[0])),    // I would like to see some safer way than casting after a read
-    switchMap((jwk) => zip(of(jwk.alg), of(jwk.kid), from(parseJwk(jwk)))),
-  );
-
-  private signJwtPayload = (jwtPayload: JWTPayload) => zip(of(jwtPayload), this.getSigningKit()).pipe(
-    switchMap(([ payload, [ alg, kid, key ] ]) => from(
-      new SignJWT(payload)
-        .setProtectedHeader({ alg, kid, typ: 'at+jwt'  })
-        .setIssuer(this.proxyUrl)
-        .sign(key),
-    )),
-  );
-
-  private createClaimExtendedToken(responseBody: string): Observable<string> {
-    const parsedBody = JSON.parse(responseBody);
-    // split the access token into header, payload, and footer parts, then get the payload
-    const accessTokenPayload = parsedBody.access_token.split('.')[1];
-    // base64url decode the access token payload
-    const decodedAccessTokenPayload = JSON.parse(decode(accessTokenPayload).toString());
+  private addAudienceClaimToToken(token: { header: any; payload: any }): Observable<{ header: any; payload: any }> {
     // set the aud claim
-    if (Array.isArray(decodedAccessTokenPayload.aud) && !decodedAccessTokenPayload.aud.includes('solid')) {
-      decodedAccessTokenPayload.aud.push('solid');
-    } else {
-      decodedAccessTokenPayload.aud = [ decodedAccessTokenPayload.aud, 'solid' ];
+    if (token.payload.aud !== 'solid'){
+      if (Array.isArray(token.payload.aud)) {
+        if (!token.payload.aud.includes('solid')){
+          token.payload.aud.push('solid');
+        }
+      } else {
+        token.payload.aud = [ token.payload.aud, 'solid' ];
+      }
     }
 
-    return this.signJwtPayload(decodedAccessTokenPayload);
+    return of(token);
   }
 
   private createAccessTokenResponse(
     response: HttpHandlerResponse,
-    claimExtendedAccessToken: string,
-    corsOrigin: string,
+    claimExtendedAccessToken: { header: any; payload: any },
   ) {
-    const parsedBody = JSON.parse(response.body);
-    parsedBody.access_token = claimExtendedAccessToken;
+    response.body.access_token = claimExtendedAccessToken;
     return of({
-      body: JSON.stringify(parsedBody),
-      headers: {
-        'access-control-allow-origin': corsOrigin,
-        'Content-Type':  'application/json',
-      },
+      body: response.body,
+      headers: {},
       status: 200,
     });
   }
 
   /**
-   * Returns true if the context is valid.
-   * Returns false if the context, it's request, or the request's method, headers, or url are not included.
+   * Returns true if the response is defined and contains an access_token with a payload. Otherwise, returns false.
    *
-   * @param {HttpHandlerContext} context
+   * @param {HttpHandlerResponse} response
    */
-  canHandle(context: HttpHandlerContext) {
-    return context
-      && context.request
-      && context.request.method
-      && context.request.headers
-      && context.request.url
+  canHandle(response: HttpHandlerResponse) {
+    return response && response.body.access_token && response.body.access_token.payload
       ? of(true)
       : of(false);
   }
