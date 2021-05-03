9a178b1a0aec778c4237f7ffe5c85714937b0992
Abel <abelfrans@gmail.com>
2021-05-03 11:35:36 +0200

    chore: refactor opaque handler + tests

--- packages/dgt-id-proxy/lib/server/opaque-access-token.handler.spec.ts
+++ packages/dgt-id-proxy/lib/server/opaque-access-token.handler.spec.ts
@@ -1,6 +1,4 @@
-import { readFile } from 'fs/promises';
-import { HttpHandler, HttpHandlerContext } from '@digita-ai/handlersjs-http';
-import { of } from 'rxjs';
+import { HttpHandlerResponse } from '@digita-ai/handlersjs-http';
 import { generateKeyPair } from 'jose/util/generate_key_pair';
 import { SignJWT } from 'jose/jwt/sign';
 import { decode } from 'jose/util/base64url';
@@ -28,8 +26,7 @@ jest.mock('fs/promises', () => {
 
 describe('OpaqueAccessTokenHandler', () => {
   let handler: OpaqueAccessTokenHandler;
-  let nestedHandler: HttpHandler;
-  let context: HttpHandlerContext;
+  let response: HttpHandlerResponse;
 
   const mockedIdToken = async () => {
     const keyPair = await generateKeyPair('ES256');
@@ -50,156 +47,61 @@ describe('OpaqueAccessTokenHandler', () => {
   };
 
   beforeEach(() => {
-    nestedHandler = {
-      handle: jest.fn(),
-      canHandle: jest.fn(),
-      safeHandle: jest.fn(),
+    handler = new OpaqueAccessTokenHandler();
+    response = {
+      body: 'mockbody',
+      headers: {},
+      status: 200,
     };
-    handler = new OpaqueAccessTokenHandler(nestedHandler, 'assets/jwks.json', 'http://localhost:3003');
-    context = { request: { headers: { 'origin': 'http://localhost' }, method: 'POST', url: new URL('http://digita.ai/') } };
   });
 
   it('should be correctly instantiated', () => {
     expect(handler).toBeTruthy();
   });
 
-  it('should error when no handler or proxyUrl is provided in the constructor', () => {
-    expect(() => new OpaqueAccessTokenHandler(undefined, 'assets/jwks.json', 'http://localhost:3003')).toThrow('A handler must be provided');
-    expect(() => new OpaqueAccessTokenHandler(null, 'assets/jwks.json', 'http://localhost:3003')).toThrow('A handler must be provided');
-    expect(() => new OpaqueAccessTokenHandler(nestedHandler, undefined, 'http://localhost:3003')).toThrow('A pathToJwks must be provided');
-    expect(() => new OpaqueAccessTokenHandler(nestedHandler, null, 'http://localhost:3003')).toThrow('A pathToJwks must be provided');
-    expect(() => new OpaqueAccessTokenHandler(nestedHandler, 'assets/jwks.json', undefined)).toThrow('A proxyUrl must be provided');
-    expect(() => new OpaqueAccessTokenHandler(nestedHandler, 'assets/jwks.json', null)).toThrow('A proxyUrl must be provided');
-
-  });
-
   describe('handle', () => {
-    it('should error when no context was provided', async () => {
-      await expect(() => handler.handle(undefined).toPromise()).rejects.toThrow('Context cannot be null or undefined');
-      await expect(() => handler.handle(null).toPromise()).rejects.toThrow('Context cannot be null or undefined');
+    it('should error when no response was provided', async () => {
+      await expect(() => handler.handle(undefined).toPromise()).rejects.toThrow('response cannot be null or undefined');
+      await expect(() => handler.handle(null).toPromise()).rejects.toThrow('response cannot be null or undefined');
     });
 
-    it('should error when no context request is provided', async () => {
-      context.request = null;
-      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No request was included in the context');
-      context.request = undefined;
-      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No request was included in the context');
+    it('should return the response unedited when the upstream server returns a response with status other than 200', async () => {
+      response.status = 400;
+
+      await expect(handler.handle(response).toPromise()).resolves.toEqual(response);
     });
 
-    it('should error when no context request method is provided', async () => {
-      context.request.method = null;
-      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No method was included in the request');
-      context.request.method = undefined;
-      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No method was included in the request');
-    });
-
-    it('should error when no context request headers are provided', async () => {
-      context.request.headers = null;
-      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No headers were included in the request');
-      context.request.headers = undefined;
-      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No headers were included in the request');
-    });
-
-    it('should error when no context request url is provided', async () => {
-      context.request.url = null;
-      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No url was included in the request');
-      context.request.url = undefined;
-      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No url was included in the request');
-    });
-
-    it('should error when method is not OPTIONS or POST', async () => {
-      context.request.method = 'GET';
-      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('this method is not supported.');
-    });
-
-    it('should return the response of the nestedHandler unedited when method is OPTIONS', async () => {
-      nestedHandler.handle = jest.fn().mockReturnValueOnce(of({ body: 'options', headers: {}, status: 200 }));
-      context.request.method = 'OPTIONS';
-      await expect(handler.handle(context).toPromise()).resolves.toEqual({ body: 'options', headers: {}, status: 200 });
-    });
-
-    it('should return an error response when the upstream server returns a response with status other than 200', async () => {
-      nestedHandler.handle = jest.fn().mockReturnValueOnce(of({
-        body: JSON.stringify({ error: 'invalid_request', error_description: 'grant request invalid' }),
-        headers: {},
-        status: 400,
-      }));
-
-      await expect(handler.handle(context).toPromise()).resolves.toEqual({
-        body: JSON.stringify({ error: 'invalid_request', error_description: 'grant request invalid' }),
-        headers: {},
-        status: 400,
-      });
-    });
-
-    it('should return a token with the issuer set to the proxy and the aud and sub claims of the id token. Header typ should be "at+jwt"', async () => {
+    it('should return a token with the issuer set to the proxy and the aud, sub, iat and exp claims of the id token. Header typ should be "at+jwt"', async () => {
       // mocked response with audience as a string (single item)
       const idToken = await mockedIdToken();
-      nestedHandler.handle = jest.fn().mockReturnValueOnce(of({
-        body: JSON.stringify({
-          access_token: 'opaqueaccesstoken',
-          id_token: idToken,
-          expires_in: 7200,
-          scope: '',
-          token_type: 'Bearer',
-        }),
-        headers: {},
-        status: 200,
-      }));
+      response.body = JSON.stringify({
+        access_token: 'opaqueaccesstoken',
+        id_token: idToken,
+        expires_in: 7200,
+        scope: '',
+        token_type: 'Bearer',
+      });
       const decodedIdTokenPayload = JSON.parse(decode(idToken.split('.')[1]).toString());
 
-      const resp = await handler.handle(context).toPromise();
+      const resp = await handler.handle(response).toPromise();
       expect(resp.status).toEqual(200);
 
-      const parsedBody = JSON.parse(resp.body);
-      expect(parsedBody.access_token).toBeDefined();
-
-      const tokenPayload = JSON.parse(decode(parsedBody.access_token.split('.')[1]).toString());
-      expect(tokenPayload.iss).toEqual('http://localhost:3003');
-      expect(tokenPayload.aud).toEqual(decodedIdTokenPayload.aud);
-      expect(tokenPayload.sub).toEqual(decodedIdTokenPayload.sub);
-
-      const tokenHeader = JSON.parse(decode(parsedBody.access_token.split('.')[0]).toString());
-      expect(tokenHeader.typ).toEqual('at+jwt');
+      expect(resp.body.access_token).toBeDefined();
+      expect(resp.body.access_token.payload.aud).toEqual(decodedIdTokenPayload.aud);
+      expect(resp.body.access_token.payload.sub).toEqual(decodedIdTokenPayload.sub);
+      expect(resp.body.access_token.payload.iat).toEqual(decodedIdTokenPayload.iat);
+      expect(resp.body.access_token.payload.exp).toEqual(decodedIdTokenPayload.exp);
 
     });
 
     describe('canHandle', () => {
-      it('should return false if no context was provided', async () => {
+      it('should return false if no response was provided', async () => {
         await expect(handler.canHandle(undefined).toPromise()).resolves.toEqual(false);
         await expect(handler.canHandle(null).toPromise()).resolves.toEqual(false);
       });
 
-      it('should return false if context was provided', async () => {
-        context.request = undefined;
-        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
-        context.request = null;
-        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
-      });
-
-      it('should return false when no context request method is provided', async () => {
-        context.request.method = null;
-        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
-        context.request.method = undefined;
-        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
-      });
-
-      it('should return false when no context request headers are provided', async () => {
-        context.request.headers = null;
-        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
-        context.request.headers = undefined;
-        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
-      });
-
-      it('should return false when no context request url is provided', async () => {
-        context.request.url = null;
-        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
-        context.request.url = undefined;
-        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
-      });
-
-      it('should return true if correct context was provided', async () => {
-        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(true);
+      it('should return true if correct response was provided', async () => {
+        await expect(handler.canHandle(response).toPromise()).resolves.toEqual(true);
       });
     });
   });
--- packages/dgt-id-proxy/lib/server/opaque-access-token.handler.ts
+++ packages/dgt-id-proxy/lib/server/opaque-access-token.handler.ts
@@ -1,113 +1,37 @@
-import { readFile } from 'fs/promises';
-import { join } from 'path';
-import { HttpHandler, HttpHandlerContext, HttpHandlerResponse, MethodNotAllowedHttpError } from '@digita-ai/handlersjs-http';
-import { Observable, of, from, throwError, zip } from 'rxjs';
-import { switchMap, catchError } from 'rxjs/operators';
+import { HttpHandlerResponse } from '@digita-ai/handlersjs-http';
+import { Observable, of, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { decode } from 'jose/util/base64url';
-import { JWK, JWTPayload } from 'jose/webcrypto/types';
-import { SignJWT } from 'jose/jwt/sign';
-import { parseJwk } from 'jose/jwk/parse';
-import { v4 as uuid }  from 'uuid';
+import { Handler } from '@digita-ai/handlersjs-core';
 
 /**
- * A {HttpHandler} that handles Access Token requests for an upstream server that returns Opaque Access Tokens
+ * A {HttpHandler} that handles Access Token responses for an upstream server that returns Opaque Access Tokens
  * by turning them into valid JSON Web Tokens
  */
-export class OpaqueAccessTokenHandler extends HttpHandler {
+export class OpaqueAccessTokenHandler extends Handler<HttpHandlerResponse, HttpHandlerResponse> {
   /**
-   * Creates an {OpaqueAccessTokenHandler} passing requests through the given handler.
-   *
-   * @param {HttpHandler} handler - the handler through which to pass incoming requests.
-   * @param {string} pathToJwks - the path to a json file containing jwks.
-   * @param {string} proxyUrl - the url of the upstream server.
-   */
-  constructor(private handler: HttpHandler, private pathToJwks: string, private proxyUrl: string) {
-    super();
-
-    if (!handler) {
-      throw new Error('A handler must be provided');
-    }
-
-    if(!pathToJwks){
-      throw new Error('A pathToJwks must be provided');
-    }
-
-    if(!proxyUrl){
-      throw new Error('A proxyUrl must be provided');
-    }
-  }
-  /**
-   * handles the context's incoming request. If the request is valid it is passed to the nested handler.
-   * If the request's method is POST the response is transformed. The sub and aud claims from the id_token returned
+   * handles the response. The sub, aud, iat and exp claims from the id_token returned
    * by the upstream server are used to create a valid JWT Access Token.
    *
-   * @param {HttpHandlerContext} context
+   * @param {HttpHandlerResponse} response
    */
-  handle(context: HttpHandlerContext) {
-    if (!context) {
-      return throwError(new Error('Context cannot be null or undefined'));
+  handle(response: HttpHandlerResponse) {
+    if (!response) {
+      return throwError(new Error('response cannot be null or undefined'));
     }
 
-    if (!context.request) {
-      return throwError(new Error('No request was included in the context'));
+    if (response.status !== 200) {
+      return of(response);
     }
 
-    if (!context.request.method) {
-      return throwError(new Error('No method was included in the request'));
-    }
-
-    if (!context.request.headers) {
-      return throwError(new Error('No headers were included in the request'));
-    }
-
-    if (!context.request.url) {
-      return throwError(new Error('No url was included in the request'));
-    }
-
-    if (context.request.method === 'OPTIONS') {
-      return this.handler.handle(context);
-    }
-
-    if (context.request.method !== 'POST') {
-      return throwError(new MethodNotAllowedHttpError('this method is not supported.'));
-    }
-
-    return this.getUpstreamResponse(context).pipe(
-      // creates a jwt access token from claims int he id token
-      switchMap((response) => zip(of(response), this.createJwtAccessToken(response.body))),
+    // creates a jwt access token from claims in the id token
+    return this.createJwtAccessToken(response.body).pipe(
       // create a response to replace the opaque access token with the jwt acces token
-      switchMap(([ response, token ]) => this.createAccessTokenResponse(
-        response,
-        token,
-        context.request.headers.origin,
-      )),
-      // switches any errors with body into responses; all the rest are server errors which will hopefully be caught higher
-      catchError((error) => error.body && error.headers && error.status ? of(error) : throwError(error)),
+      map((token) => this.createAccessTokenResponse(response, token)),
     );
   }
 
-  private getUpstreamResponse = (context: HttpHandlerContext) => this.handler.handle(context).pipe(
-    switchMap((response) => response.status === 200 ? of(response) : throwError(response)),
-  );
-
-  private getSigningKit = () => from(readFile(join(process.cwd(), this.pathToJwks))).pipe(
-    switchMap<Buffer, JWK>((keyFile) => of(JSON.parse(keyFile.toString()).keys[0])),
-    switchMap((jwk) => zip(of(jwk.alg), of(jwk.kid), from(parseJwk(jwk)))),
-  );
-
-  private signJwtPayload = (jwtPayload: JWTPayload) => zip(of(jwtPayload), this.getSigningKit()).pipe(
-    switchMap(([ payload, [ alg, kid, key ] ]) => from(
-      new SignJWT(payload)
-        .setProtectedHeader({ alg, kid, typ: 'at+jwt'  })
-        .setExpirationTime('2h')
-        .setIssuedAt()
-        .setJti(uuid())
-        .setIssuer(this.proxyUrl)
-        .sign(key),
-    )),
-  );
-
-  private createJwtAccessToken(responseBody: string): Observable<string> {
+  private createJwtAccessToken(responseBody: string): Observable<{ header: any; payload: any }> {
     const parsedBody = JSON.parse(responseBody);
     // split the id token into header, payload, and footer parts, then get the payload
     const idTokenPayload = parsedBody.id_token.split('.')[1];
@@ -115,45 +39,39 @@ export class OpaqueAccessTokenHandler extends HttpHandler {
     const decodedIdTokenPayload = JSON.parse(decode(idTokenPayload).toString());
 
     // get the sub and aud claims from the id token and add them to the accessTokenPayload
-    const accessTokenPayload = {
-      sub: decodedIdTokenPayload.sub,
-      aud: decodedIdTokenPayload.aud,
+    const accessToken = {
+      header: {},
+      payload: {
+        sub: decodedIdTokenPayload.sub,
+        aud: decodedIdTokenPayload.aud,
+        iat: decodedIdTokenPayload.iat,
+        exp: decodedIdTokenPayload.exp,
+      },
     };
 
     // sign the token
-    return this.signJwtPayload(accessTokenPayload);
+    return of(accessToken);
   }
 
   private createAccessTokenResponse(
     response: HttpHandlerResponse,
-    jwtAccessToken: string,
-    corsOrigin: string,
+    jwtAccessToken: { header: any; payload: any },
   ) {
     const parsedBody = JSON.parse(response.body);
     parsedBody.access_token = jwtAccessToken;
-    return of({
-      body: JSON.stringify(parsedBody),
-      headers: {
-        'access-control-allow-origin': corsOrigin,
-        'Content-Type':  'application/json',
-      },
+    return {
+      body: parsedBody,
+      headers: {},
       status: 200,
-    });
+    };
   }
   /**
-   * Returns true if the context is valid.
-   * Returns false if the context, it's request, or the request's method, headers, or url are not included.
+   * Returns true if the response is defined. Otherwise, returns false.
    *
-   * @param {HttpHandlerContext} context
+   * @param {HttpHandlerResponse} response
    */
-  canHandle(context: HttpHandlerContext) {
-    return context
-      && context.request
-      && context.request.method
-      && context.request.headers
-      && context.request.url
-      ? of(true)
-      : of(false);
+  canHandle(response: HttpHandlerResponse) {
+    return response ? of(true) : of(false);
   }
 
 }
