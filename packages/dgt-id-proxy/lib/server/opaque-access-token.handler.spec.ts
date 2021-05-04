5167f49f3e9aecd5463bd82400a431bcb298d729
Abel <abelfrans@gmail.com>
2021-05-04 16:00:23 +0200

    feat: handler uses client_id from reqbody as claim

--- packages/dgt-id-proxy/lib/server/opaque-access-token.handler.spec.ts
+++ packages/dgt-id-proxy/lib/server/opaque-access-token.handler.spec.ts
@@ -1,4 +1,5 @@
-import { HttpHandlerResponse } from '@digita-ai/handlersjs-http';
+import { HttpHandler, HttpHandlerContext } from '@digita-ai/handlersjs-http';
+import { of } from 'rxjs';
 import { generateKeyPair } from 'jose/util/generate_key_pair';
 import { SignJWT } from 'jose/jwt/sign';
 import { decode } from 'jose/util/base64url';
@@ -26,7 +27,8 @@ jest.mock('fs/promises', () => {
 
 describe('OpaqueAccessTokenHandler', () => {
   let handler: OpaqueAccessTokenHandler;
-  let response: HttpHandlerResponse;
+  let nestedHandler: HttpHandler;
+  let context: HttpHandlerContext;
 
   const mockedIdToken = async () => {
     const keyPair = await generateKeyPair('ES256');
@@ -47,43 +49,101 @@ describe('OpaqueAccessTokenHandler', () => {
   };
 
   beforeEach(() => {
-    handler = new OpaqueAccessTokenHandler();
-    response = {
-      body: 'mockbody',
-      headers: {},
-      status: 200,
+    nestedHandler = {
+      handle: jest.fn(),
+      canHandle: jest.fn(),
+      safeHandle: jest.fn(),
     };
+    handler = new OpaqueAccessTokenHandler(nestedHandler);
+    context = { request: { headers: {}, method: 'POST', url: new URL('http://digita.ai/'), body: 'client_id=mockClient' } };
   });
 
   it('should be correctly instantiated', () => {
     expect(handler).toBeTruthy();
   });
 
+  it('should error when no handler is provided', () => {
+    expect(() => new OpaqueAccessTokenHandler(undefined)).toThrow('handler must be defined');
+    expect(() => new OpaqueAccessTokenHandler(null)).toThrow('handler must be defined');
+  });
+
   describe('handle', () => {
-    it('should error when no response was provided', async () => {
-      await expect(() => handler.handle(undefined).toPromise()).rejects.toThrow('response cannot be null or undefined');
-      await expect(() => handler.handle(null).toPromise()).rejects.toThrow('response cannot be null or undefined');
+    it('should error when no context was provided', async () => {
+      await expect(() => handler.handle(undefined).toPromise()).rejects.toThrow('Context cannot be null or undefined');
+      await expect(() => handler.handle(null).toPromise()).rejects.toThrow('Context cannot be null or undefined');
     });
 
-    it('should return the response unedited when the upstream server returns a response with status other than 200', async () => {
-      response.status = 400;
-
-      await expect(handler.handle(response).toPromise()).resolves.toEqual(response);
+    it('should error when no context request is provided', async () => {
+      context.request = null;
+      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No request was included in the context');
+      context.request = undefined;
+      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No request was included in the context');
     });
 
-    it('should return a token with the issuer set to the proxy and the aud, sub, iat and exp claims of the id token. Header typ should be "at+jwt"', async () => {
+    it('should error when no context request method is provided', async () => {
+      context.request.method = null;
+      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No method was included in the request');
+      context.request.method = undefined;
+      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No method was included in the request');
+    });
+
+    it('should error when no context request headers are provided', async () => {
+      context.request.headers = null;
+      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No headers were included in the request');
+      context.request.headers = undefined;
+      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No headers were included in the request');
+    });
+
+    it('should error when no context request url is provided', async () => {
+      context.request.url = null;
+      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No url was included in the request');
+      context.request.url = undefined;
+      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No url was included in the request');
+    });
+
+    it('should error when no context request body is provided', async () => {
+      context.request.body = null;
+      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No body was included in the request');
+      context.request.body = undefined;
+      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('No body was included in the request');
+    });
+
+    it('should error when no request body does not contain a client_id is provided', async () => {
+      context.request.body = 'noClientId';
+      await expect(() => handler.handle(context).toPromise()).rejects.toThrow('Request body must contain a client_id claim');
+    });
+
+    it('should return an error response when the upstream server returns a response with status other than 200', async () => {
+      nestedHandler.handle = jest.fn().mockReturnValueOnce(of({
+        body: JSON.stringify({ error: 'invalid_request', error_description: 'grant request invalid' }),
+        headers: {},
+        status: 400,
+      }));
+
+      await expect(handler.handle(context).toPromise()).resolves.toEqual({
+        body: JSON.stringify({ error: 'invalid_request', error_description: 'grant request invalid' }),
+        headers: {},
+        status: 400,
+      });
+    });
+
+    it('should return a token with the issuer set to the proxy and the aud, sub, iat and exp claims of the id token', async () => {
       // mocked response with audience as a string (single item)
       const idToken = await mockedIdToken();
-      response.body = JSON.stringify({
-        access_token: 'opaqueaccesstoken',
-        id_token: idToken,
-        expires_in: 7200,
-        scope: '',
-        token_type: 'Bearer',
-      });
+      nestedHandler.handle = jest.fn().mockReturnValueOnce(of({
+        body: JSON.stringify({
+          access_token: 'opaqueaccesstoken',
+          id_token: idToken,
+          expires_in: 7200,
+          scope: '',
+          token_type: 'Bearer',
+        }),
+        headers: {},
+        status: 200,
+      }));
       const decodedIdTokenPayload = JSON.parse(decode(idToken.split('.')[1]).toString());
 
-      const resp = await handler.handle(response).toPromise();
+      const resp = await handler.handle(context).toPromise();
       expect(resp.status).toEqual(200);
 
       expect(resp.body.access_token).toBeDefined();
@@ -91,18 +151,52 @@ describe('OpaqueAccessTokenHandler', () => {
       expect(resp.body.access_token.payload.sub).toEqual(decodedIdTokenPayload.sub);
       expect(resp.body.access_token.payload.iat).toEqual(decodedIdTokenPayload.iat);
       expect(resp.body.access_token.payload.exp).toEqual(decodedIdTokenPayload.exp);
-      expect(resp.body.access_token.payload.client_id).toEqual(decodedIdTokenPayload.aud);
+      expect(resp.body.access_token.payload.client_id).toEqual('mockClient');
 
     });
 
     describe('canHandle', () => {
-      it('should return false if no response was provided', async () => {
+      it('should return false if no context was provided', async () => {
         await expect(handler.canHandle(undefined).toPromise()).resolves.toEqual(false);
         await expect(handler.canHandle(null).toPromise()).resolves.toEqual(false);
       });
 
-      it('should return true if correct response was provided', async () => {
-        await expect(handler.canHandle(response).toPromise()).resolves.toEqual(true);
+      it('should return false if context was provided', async () => {
+        context.request = undefined;
+        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
+        context.request = null;
+        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
+      });
+
+      it('should return false when no context request method is provided', async () => {
+        context.request.method = null;
+        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
+        context.request.method = undefined;
+        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
+      });
+
+      it('should return false when no context request headers are provided', async () => {
+        context.request.headers = null;
+        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
+        context.request.headers = undefined;
+        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
+      });
+
+      it('should return false when no context request url is provided', async () => {
+        context.request.url = null;
+        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
+        context.request.url = undefined;
+        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
+      });
+      it('should return false when no context request body is provided', async () => {
+        context.request.body = null;
+        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
+        context.request.body = undefined;
+        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(false);
+      });
+
+      it('should return true if correct context was provided', async () => {
+        await expect(handler.canHandle(context).toPromise()).resolves.toEqual(true);
       });
     });
   });
