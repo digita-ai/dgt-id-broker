8a644756693322ce1261dee028163314f5df175d
Abel <abelfrans@gmail.com>
2021-05-04 14:48:25 +0200

    chore: made changes based on review by @woutermont

--- packages/dgt-id-proxy/lib/util/generate-keys.ts
+++ packages/dgt-id-proxy/lib/util/generate-keys.ts
@@ -1,6 +1,8 @@
-// This script will generate an EC ES256 key and an RSA RS256 key
-
-// usage: npm run generate-keys [-- [relative path to file (optional)]]
+/* eslint-disable no-console -- is cli script */
+/**
+ * This script will generate an EC ES256 key and an RSA RS256 key
+ * Usage: npm run generate:keys [-- [relative path to file (optional)]]
+ */
 
 import { writeFileSync } from 'fs';
 import { join } from 'path';
@@ -8,43 +10,34 @@ import { generateKeyPair } from 'jose/util/generate_key_pair';
 import { fromKeyLike } from 'jose/jwk/from_key_like';
 import { v4 as uuid } from 'uuid';
 
-// get the arguments
 const args = process.argv.slice(2);
-let filePath = args[0];
-if (!filePath) {
-  // use this path by default if no filePath was provided
-  filePath = 'assets/jwks.json';
-}
+const filePath = args[0] ?? 'assets/jwks.json';
 
 const generateKeys = async () => {
-  // generate an ES256 key
+
   const ecKey =  await generateKeyPair('ES256');
-  // get the private key in JWK format
   const ecJwk = await fromKeyLike(ecKey.privateKey);
-  // add missing claims
   ecJwk.kid = uuid();
   ecJwk.alg = 'ES256';
   ecJwk.use = 'sig';
-  // generate an RS256 key
+
   const rsaKey =  await generateKeyPair('RS256');
-  // get the private key in JWK format
   const rsaJwk = await fromKeyLike(rsaKey.privateKey);
-  // add missing claims
   rsaJwk.kid = uuid();
   rsaJwk.alg = 'RS256';
   rsaJwk.use = 'sig';
 
-  // create the format in which the jwks must be saved
   const jwks = {
     'keys': [
       ecJwk,
       rsaJwk,
     ],
   };
-  // write the jwks to the given file
+
   writeFileSync(join(process.cwd(), filePath), JSON.stringify(jwks));
-  // eslint-disable-next-line no-console
+
   console.log(`Successfully wrote the keys to "${join(process.cwd(), filePath)}"`);
+
 };
 
 generateKeys();
--- packages/dgt-id-proxy/lib/util/generate-openid-configuration.ts
+++ packages/dgt-id-proxy/lib/util/generate-openid-configuration.ts
@@ -1,47 +1,39 @@
-// This script uses the openid-configuration of the specified upstream server and replaces it's urls with that of the specified proxy url
-
-// usage: npm run generate-config -- [upstream server issuer url] [proxy issuer url] [relative path to file (optional)]
+/* eslint-disable no-console -- is cli script */
+/**
+ * This script uses the openid-configuration of the specified upstream server and replaces it's urls with that of the specified proxy url.
+ * Usage: npm run generate:oidc -- [upstream server issuer url] [proxy issuer url] [relative path to file (optional)]
+ */
 import { writeFileSync } from 'fs';
 import { join } from 'path';
 import fetch from 'node-fetch';
 
-// get the arguments
 const args = process.argv.slice(2);
 const upstreamUrl = args[0];
 const proxyUrl = args[1];
-let filePath = args[2];
-if (!filePath) {
-  // use this path by default if no filePath was provided
-  filePath = 'assets/openid-configuration.json';
-}
+const filePath = args[2] ?? 'assets/openid-configuration.json';
 
 const fetchOpenidConfig = async (url: string) => {
   const response = await fetch(url);
   return response.json();
 };
 
-if (upstreamUrl.endsWith('/') || proxyUrl.endsWith('/')) {
-  // We don't want trailing slashes, otherwise the "issuer" claim will not be matched
-  // eslint-disable-next-line no-console
-  console.log('Urls should not contain a trailing \'/\'');
-} else if (args.length < 2) {
-  // We need an upstream url and a proxy url at least
-  // eslint-disable-next-line no-console
+if (args.length < 2) {
   console.log('Please provide both an upstream url and proxy url');
 } else {
-  // Fetch the config of the upstream server
-  fetchOpenidConfig(upstreamUrl + '/.well-known/openid-configuration')
-    .then((data) => {
-      for (const key of Object.keys(data)) {
-        if (typeof data[key] === 'string'){
-          // replace all instances of the upstream url with the proxy url
-          data[key] = data[key].replace(upstreamUrl, proxyUrl);
-        }
+  fetchOpenidConfig(upstreamUrl + '/.well-known/openid-configuration').then((data) => {
+
+    for (const key of Object.keys(data)) {
+      if (typeof data[key] === 'string'){
+        // replace all instances of the upstream url with the proxy url
+        data[key] = data[key].replace(upstreamUrl, proxyUrl);
       }
-      data.solid_oidc_supported = 'https://solidproject.org/TR/solid-oidc';
-      // write the data to the specified path
-      writeFileSync(join(process.cwd(), filePath), JSON.stringify(data));
-      // eslint-disable-next-line no-console
-      console.log(`Successfully wrote the config "${join(process.cwd(), filePath)}"`);
-    });
+    }
+
+    data.solid_oidc_supported = 'https://solidproject.org/TR/solid-oidc';
+
+    writeFileSync(join(process.cwd(), filePath), JSON.stringify(data));
+
+    console.log(`Successfully wrote the config "${join(process.cwd(), filePath)}"`);
+
+  });
 }
