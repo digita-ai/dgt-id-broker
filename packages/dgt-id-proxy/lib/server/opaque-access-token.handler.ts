3ec6c0e510005e3c595bd61a9282ca3a73e410fa
Abel Van den Briel <43853000+AbelVandenBriel@users.noreply.github.com>
2021-05-04 16:06:38 +0200

    fix: add client-id claim in opaque access token handler (#60)
    * feat: add client-id claim
    
    * feat: handler uses client_id from reqbody as claim

--- packages/dgt-id-proxy/lib/server/opaque-access-token.handler.ts
+++ packages/dgt-id-proxy/lib/server/opaque-access-token.handler.ts
@@ -1,37 +1,69 @@
-import { HttpHandlerResponse } from '@digita-ai/handlersjs-http';
-import { Observable, of, throwError } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { HttpHandler, HttpHandlerContext, HttpHandlerResponse } from '@digita-ai/handlersjs-http';
+import { Observable, of, throwError, zip } from 'rxjs';
+import { map, switchMap, catchError } from 'rxjs/operators';
 import { decode } from 'jose/util/base64url';
-import { Handler } from '@digita-ai/handlersjs-core';
 
 /**
  * A {HttpHandler} that handles Access Token responses for an upstream server that returns Opaque Access Tokens
  * by turning them into valid JSON Web Tokens
  */
-export class OpaqueAccessTokenHandler extends Handler<HttpHandlerResponse, HttpHandlerResponse> {
+export class OpaqueAccessTokenHandler extends HttpHandler {
+
+  constructor(private handler: HttpHandler){
+    super();
+    if (!handler) {
+      throw Error('handler must be defined');
+    }
+  }
   /**
-   * handles the response. The sub, aud, iat and exp claims from the id_token returned
-   * by the upstream server are used to create a valid JWT Access Token.
+   * handles the context by saving the client_id passed in it and then getting the response from it's handler.
+   * The sub, aud, iat and exp claims from the id_token returned by the upstream server, along with the client_id,
+   * are used to create a valid JWT Access Token.
    *
-   * @param {HttpHandlerResponse} response
+   * @param {HttpHandlerContext} context
    */
-  handle(response: HttpHandlerResponse) {
-    if (!response) {
-      return throwError(new Error('response cannot be null or undefined'));
+  handle(context: HttpHandlerContext) {
+    if (!context) {
+      return throwError(new Error('Context cannot be null or undefined'));
     }
 
-    if (response.status !== 200) {
-      return of(response);
+    if (!context.request) {
+      return throwError(new Error('No request was included in the context'));
     }
 
-    // creates a jwt access token from claims in the id token
-    return this.createJwtAccessToken(response.body).pipe(
-      // create a response to replace the opaque access token with the jwt acces token
-      map((token) => this.createAccessTokenResponse(response, token)),
+    if (!context.request.method) {
+      return throwError(new Error('No method was included in the request'));
+    }
+
+    if (!context.request.headers) {
+      return throwError(new Error('No headers were included in the request'));
+    }
+
+    if (!context.request.url) {
+      return throwError(new Error('No url was included in the request'));
+    }
+
+    if (!context.request.body) {
+      return throwError(new Error('No body was included in the request'));
+    }
+
+    const client_id = new URLSearchParams(context.request.body).get('client_id');
+    if (!client_id) {
+      return throwError(new Error('Request body must contain a client_id claim'));
+    }
+
+    return this.getUpstreamResponse(context).pipe(
+      switchMap((response) => zip(of(response), this.createJwtAccessToken(response.body, client_id))),
+      map(([ response, token ]) => this.createAccessTokenResponse(response, token)),
+      catchError((error) => error.body && error.headers && error.status ? of(error) : throwError(error)),
     );
   }
 
-  private createJwtAccessToken(responseBody: string): Observable<{ header: any; payload: any }> {
+  private getUpstreamResponse = (context: HttpHandlerContext) => this.handler.handle(context).pipe(
+    switchMap((response) => response.status === 200 ? of(response) : throwError(response)),
+  );
+
+  private createJwtAccessToken(responseBody: string, client_id: string): Observable<{ header: any; payload: any }> {
     const parsedBody = JSON.parse(responseBody);
     // split the id token into header, payload, and footer parts, then get the payload
     const idTokenPayload = parsedBody.id_token.split('.')[1];
@@ -47,6 +79,7 @@ export class OpaqueAccessTokenHandler extends Handler<HttpHandlerResponse, HttpH
         aud: decodedIdTokenPayload.aud,
         iat: decodedIdTokenPayload.iat,
         exp: decodedIdTokenPayload.exp,
+        client_id,
       },
     };
 
@@ -65,14 +98,22 @@ export class OpaqueAccessTokenHandler extends Handler<HttpHandlerResponse, HttpH
       status: 200,
     };
   }
-  /**
-   * Returns true if the response is defined. Otherwise, returns false.
-   *
-   * @param {HttpHandlerResponse} response
-   */
-  canHandle(response: HttpHandlerResponse) {
-    return response ? of(true) : of(false);
-  }
 
+  /**
+   * Returns true if the context is valid.
+   * Returns false if the context, it's request, or the request's method, headers, url or body are not included.
+   *
+   * @param {HttpHandlerContext} context
+   */
+  canHandle(context: HttpHandlerContext) {
+    return context
+        && context.request
+        && context.request.method
+        && context.request.headers
+        && context.request.url
+        && context.request.body
+      ? of(true)
+      : of(false);
+  }
 }
 
