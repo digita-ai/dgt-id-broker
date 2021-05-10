5b9d0af481b996d529ed80c0a709e5064c5cff7d
Abel <abelfrans@gmail.com>
2021-05-10 11:16:17 +0200

    feat: support launch variables WIP

--- packages/dgt-id-proxy/lib/main.ts
+++ packages/dgt-id-proxy/lib/main.ts
@@ -1,6 +1,8 @@
 import * as path from 'path';
 import { ComponentsManager } from 'componentsjs';
 import { NodeHttpServer } from '@digita-ai/handlersjs-http';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
 /**
  * Instantiates a server from the passed configuration and starts it.
@@ -25,6 +27,29 @@ export const launch: (variables: Record<string, any>) => Promise<void> = async (
 
   const server: NodeHttpServer = await manager.instantiate('urn:handlersjs-http:default:NodeHttpServer', { variables });
   server.start();
+};
+
+export const createVariables: (args: string[]) => Record<string, any> = (args: string[]): Record<string, any> => {
+  const { argv: params } = yargs(hideBin(args))
+    .usage('node ./dist/main.js [args]')
+    .options({
+      config: { type: 'string', alias: 'c' },
+      baseUri: { type: 'string', alias: 'b' },
+      cacheUri: { type: 'string', alias: 'cu' },
+      publicUri: { type: 'string', alias: 'pu' },
+      port: { type: 'number', alias: 'p' },
+      mainModulePath: { type: 'string', alias: 'm' },
+    })
+    .help();
+
+  return {
+    'urn:dgt-platform-api:variables:customConfigPath': params.config,
+    'urn:dgt-platform-api:variables:mainModulePath': params.mainModulePath,
+    'urn:dgt-platform-api:variables:baseUri': params.baseUri ? params.baseUri : 'http://localhost:3000/',
+    'urn:dgt-platform-api:variables:cacheUri': params.cacheUri ? params.cacheUri : 'http://localhost:3001/sparql/',
+    'urn:dgt-platform-api:variables:publicUri': params.publicUri ? params.publicUri : 'http://localhost:4203/',
+    'urn:dgt-platform-api:variables:port': params.port ? params.port : '3000',
+  };
 };
 
 launch({});
