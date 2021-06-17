553d0fd735e6871c1b30514850340c23ef7be60d
Wouter Termont <woutermont@gmail.com>
2021-06-17 17:34:59 +0200

    feat: solid service
    Signed-off-by: Wouter Termont <woutermont@gmail.com>

---
+++ packages/dgt-id-kit/lib/index.ts
@@ -0,0 +1,6 @@
+export * from './models/issuer.model';
+export * from './models/profile.model';
+export * from './models/resource.model';
+export * from './models/session.model';
+export * from './solid-sdk.service';
+export * from './solid.service';
