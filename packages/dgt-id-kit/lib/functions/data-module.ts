c7712a1fd1b494a164f4aed2cb327569ee677e3d
Arthur Joppart <arthur@digita.ai>
2021-07-27 16:13:04 +0200

    feat: added doc to the functions

--- packages/dgt-id-kit/lib/functions/data-module.ts
+++ packages/dgt-id-kit/lib/functions/data-module.ts
@@ -1,6 +1,12 @@
 import { Quad } from 'rdf-js';
 import { Parser, Quad as N3Quad } from 'n3';
 
+/**
+ * Transform all data from a turtle file to a list of Quads
+ *
+ * @param url the url of the turtle file to convert to Quads
+ * @returns an array of Quads from the turtle file
+ */
 export const getTurtleFileAsQuads = async (url: URL): Promise<Quad[]> => {
 
   if (!url) {
--- packages/dgt-id-kit/lib/functions/web-id-module.ts
+++ packages/dgt-id-kit/lib/functions/web-id-module.ts
@@ -2,6 +2,12 @@ import { Quad } from 'rdf-js';
 import { Issuer } from '../models/issuer.model';
 import { getTurtleFileAsQuads } from './data-module';
 
+/**
+ * Transform all data from a profile to a list of Quads
+ *
+ * @param webid the webid to convert to Quads
+ * @returns an array of Quads from the profile
+ */
 export const getWebIdProfile = async (webid: URL): Promise<Quad[]> => {
 
   if (!webid) {
@@ -22,6 +28,12 @@ export const getWebIdProfile = async (webid: URL): Promise<Quad[]> => {
 
 };
 
+/**
+ * Get an Issuer instance created from a list of Quads, most likely from a profile
+ *
+ * @param quads list of quads
+ * @returns an Issuer instance if one was found, or undefined when no issuer was found
+ */
 export const getIssuerFromQuads = async (quads: Quad[]): Promise<Issuer | undefined> => {
 
   if (!quads) {
@@ -37,6 +49,13 @@ export const getIssuerFromQuads = async (quads: Quad[]): Promise<Issuer | undefi
 
 };
 
+/**
+ * Provided a webid, this function will return you an Issuer instance or undefined
+ * depending on whether an oidcIssuer is mentioned on the profile of this webid
+ *
+ * @param webid the webid of which you want the Issuer
+ * @returns an Issuer instance if one was found, or undefined when no issuer was found
+ */
 export const getIssuerFromWebId = async (webid: URL): Promise<Issuer | undefined> => {
 
   if (!webid) {
