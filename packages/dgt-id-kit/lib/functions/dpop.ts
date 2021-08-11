08fdc7e07059998488ce9d86608359e332781a65
Arthur Joppart <arthur@digita.ai>
2021-08-11 14:59:54 +0200

    feat: changed storage and implemented dpop generateKeys()

--- packages/dgt-id-kit/lib/functions/dpop.ts
+++ packages/dgt-id-kit/lib/functions/dpop.ts
@@ -1,17 +1,32 @@
+import { fromKeyLike } from 'jose/jwk/from_key_like';
+import { generateKeyPair } from 'jose/util/generate_key_pair';
 import { KeyGenerationAlgorithm } from '../models/key-generation-algorithm.model';
+import { store } from './storage';
 
 export const generateKeys = async (algorithm: KeyGenerationAlgorithm = 'ES256'): Promise<void> => {
 
-  console.log('generateKeys()');
+  try {
 
-  return undefined;
+    const keyPair = await generateKeyPair(algorithm);
+
+    const privateKey = keyPair.privateKey;
+    const publicKey = await fromKeyLike(keyPair.publicKey);
+
+    await store.set('privateKey', privateKey);
+    await store.set('publicKey', { publicKey, alg: algorithm });
+
+  } catch (error: unknown) {
+
+    throw new Error(`An error occurred while generating keys with algorithm ${algorithm}: ${error}`);
+
+  }
 
 };
 
-export const createDPoPProof = async (htu: string, htm: string): Promise<string> => {
+// export const createDPoPProof = async (htu: string, htm: string): Promise<string> => {
 
-  console.log('createDPoPProof()');
+//   console.log('createDPoPProof()');
 
-  return undefined;
+//   return undefined;
 
-};
+// };
--- packages/dgt-id-kit/lib/functions/storage.ts
+++ packages/dgt-id-kit/lib/functions/storage.ts
@@ -1,57 +1,9 @@
-import clone from 'clone';
-import { TypedKeyValueStore } from '@digita-ai/handlersjs-core';
+import { MemoryStore } from '@digita-ai/handlersjs-core';
+import { JWK, KeyLike } from 'jose/webcrypto/types';
 
-/**
- * A {@link KeyValueStore} which uses a JavaScript Map for internal storage.
- *
- * @inheritdoc
- */
-export class MemoryTypedKeyValueStore<M> implements TypedKeyValueStore<M> {
-
-  private readonly data: Map<keyof M, M[keyof M]>;
-
-  constructor(initialData?: [keyof M, M[keyof M]][]) {
-
-    this.data = new Map(initialData);
-
-  }
-
-  async get<T extends keyof M>(key: T): Promise<M[T] | undefined> {
-
-    return this.data.has(key) ? clone(this.data.get(key) as M[T]) : undefined;
-
-  }
-
-  async has<T extends keyof M>(key: T): Promise<boolean> {
-
-    return this.data.has(key);
-
-  }
-
-  async set<T extends keyof M>(key: T, value: M[T]): Promise<this> {
-
-    this.data.set(key, clone(value));
-
-    return this;
-
-  }
-
-  async delete<T extends keyof M>(key: T): Promise<boolean> {
-
-    return this.data.delete(key);
-
-  }
-
-  async* entries(): AsyncIterableIterator<[keyof M, M[keyof M]]> {
-
-    for (const [ key, value ] of this.data.entries()) {
-
-      yield [ key, clone(value) ];
-
-    }
-
-  }
-
+interface storeInterface  {
+  privateKey: KeyLike;
+  publicKey: JWK;
 }
 
-export const store = new MemoryTypedKeyValueStore();
+export const store = new MemoryStore<storeInterface>();
