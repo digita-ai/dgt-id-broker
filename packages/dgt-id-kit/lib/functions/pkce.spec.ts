d6c16daf83a57a51f21aa0ec74dfcdb348bfe95a
Arthur Joppart <arthur@digita.ai>
2021-08-12 16:45:55 +0200

    test: added tests for generateCodeVerifier()

--- packages/dgt-id-kit/lib/functions/pkce.spec.ts
+++ packages/dgt-id-kit/lib/functions/pkce.spec.ts
@@ -1,6 +1,50 @@
-import { base64UrlEncode } from './pkce';
+import { base64UrlEncode, generateCodeVerifier } from './pkce';
 
 describe('generateCodeVerifier()', () => {
+
+  it('should return a code verifier of the desired length', async () => {
+
+    const result = generateCodeVerifier(100);
+    await expect(result).resolves.toHaveLength(100);
+
+    const result2 = generateCodeVerifier(200);
+    await expect(result2).resolves.toHaveLength(200);
+
+  });
+
+  // it('should save the code verifier to the store', async () => {
+
+  //   const result = await generateCodeVerifier(50);
+  //   const instore = await store.get('codeVerifier');
+  //   expect(instore).toBeDefined();
+  //   expect(instore).toBe(result);
+  //   await expect(store.has('codeVerifier')).resolves.toBe(true);
+
+  // });
+
+  it('should return a code verifier that only contains valid code verifier characters', async () => {
+
+    const result = await generateCodeVerifier(100);
+    expect(result).toBeDefined();
+
+    const regex = /^[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\-\.\_\~]+$/;
+    expect(regex.test(result)).toBe(true);
+
+  });
+
+  it('should throw when parameter length is undefined', async () => {
+
+    const result = generateCodeVerifier(undefined);
+    await expect(result).rejects.toThrow('Parameter "length" should be set');
+
+  });
+
+  it('should throw when parameter length is less than 43', async () => {
+
+    const result = generateCodeVerifier(42);
+    await expect(result).rejects.toThrow('A PKCE code_verifier has to be at least 43 characters long');
+
+  });
 
 });
 
