08de00c503475a7d2dc869397183686d05967fd5
Arthur Joppart <arthur@digita.ai>
2021-08-12 15:49:21 +0200

    feat: implemented base64URLEncode()

--- packages/dgt-id-kit/lib/functions/pkce.ts
+++ packages/dgt-id-kit/lib/functions/pkce.ts
@@ -1,3 +1,4 @@
+import CryptoJS from 'crypto-js';
 
 export const generateCodeVerifier = (length: number): string => {
 
@@ -8,5 +9,11 @@ export const generateCodeChallenge = (codeVerifier: string): string => {
 };
 
 export const base64UrlEncode = (string: string): string => {
+
+  if (!string) { throw new Error('Parameter "string" should be set'); }
+
+  const hash = CryptoJS.SHA256(string);
+
+  return hash.toString(CryptoJS.enc.Base64).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
 
 };
