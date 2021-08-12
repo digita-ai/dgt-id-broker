582be326bf61cb2cb9a21fc439bc1feb76344c68
Arthur Joppart <arthur@digita.ai>
2021-08-12 15:49:58 +0200

    test: added base64UrlEncode() tests

--- packages/dgt-id-kit/lib/functions/pkce.spec.ts
+++ packages/dgt-id-kit/lib/functions/pkce.spec.ts
@@ -1,3 +1,4 @@
+import { base64UrlEncode } from './pkce';
 
 describe('generateCodeVerifier()', () => {
 
@@ -8,5 +9,17 @@ describe('generateCodeChallenge()', () => {
 });
 
 describe('base64UrlEncode()', () => {
+
+  it('should return the base64 encoded string of the given string', async () => {
+
+    expect(base64UrlEncode('string')).toBe('RzKH-CmNunFjqJeQiVj3wOrnM-JdLgJ5kuou3JvtL6g');
+
+  });
+
+  it('should throw when parameter string is undefined', async () => {
+
+    expect(() => base64UrlEncode(undefined)).toThrow('Parameter "string" should be set');
+
+  });
 
 });
