3ec6a8025a6e6fe4047822854ef572d8542dc528
Arthur Joppart <arthur@digita.ai>
2021-08-26 13:27:15 +0200

    fix: removed useless if statements

--- packages/dgt-id-kit/lib/functions/oidc.ts
+++ packages/dgt-id-kit/lib/functions/oidc.ts
@@ -127,11 +127,11 @@ export const tokenRequest = async (
 
     if (!parsed?.access_token) { throw new Error('The tokenRequest response must contain an access_token field, and it did not.'); }
 
-    if (parsed.access_token) { await store.set('accessToken', parsed.access_token); }
+    await store.set('accessToken', parsed.access_token);
 
     if (!parsed?.id_token) { throw new Error('The tokenRequest response must contain an id_token field, and it did not.'); }
 
-    if (parsed.id_token) { await store.set('idToken', parsed.id_token); }
+    await store.set('idToken', parsed.id_token);
 
     if (parsed?.refresh_token) { await store.set('refreshToken', parsed.refresh_token); }
 
@@ -192,11 +192,11 @@ export const refreshTokenRequest = async (
 
     if (!parsed?.access_token) { throw new Error('The tokenRequest response must contain an access_token field, and it did not.'); }
 
-    if (parsed.access_token) { await store.set('accessToken', parsed.access_token); }
+    await store.set('accessToken', parsed.access_token);
 
     if (!parsed?.refresh_token) { throw new Error('The tokenRequest response must contain an refresh_token field, and it did not.'); }
 
-    if (parsed.refresh_token) { await store.set('refreshToken', parsed.refresh_token); }
+    await store.set('refreshToken', parsed.refresh_token);
 
     if (parsed?.id_token) { await store.set('idToken', parsed.id_token); }
 
