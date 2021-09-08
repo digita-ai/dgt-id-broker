1c0bdced8a54d6715059de80259a64ad3f272c11
AbelVandenBriel <abel.van.den.briel@digita.ai>
2021-09-08 14:52:14 +0200

    chore: add comments to the demo

--- demo/demo-id-kit/getAccessTokenAndResource.js
+++ demo/demo-id-kit/getAccessTokenAndResource.js
@@ -3,37 +3,49 @@ import { SolidOidcClient, LocalStorageStore } from "@digita-ai/dgt-id-kit"
 // Importing the environment variables defined in the .env file.
 const env = import.meta.env
 
+// create the store
 const store = new LocalStorageStore();
 
+// create the client, but don't initialize it
 const solidOidcClient = new SolidOidcClient(store, true);
 
+// tells us if the store contains an access token and the user is "logged in"
 const loggedIn = async () => {
     const accessToken = await store.get('accessToken')
     return accessToken !== undefined
 }
 
+// check if the user is logged in
 loggedIn().then((logged) => {
+    // if they are not...
     if (!logged) {
+        // check if the url contains a code and if it does, handle the redirect, get an access token, and send a resource access request.
         if (new URLSearchParams(window.location.search).has('code')) {
             solidOidcClient.handleIncomingRedirect(`http://localhost:${env.VITE_OIDC_PORT}`, `http://${env.VITE_IP}:${env.VITE_PORT}/requests.html`).then(() => {
                 logoutButtonAndAccessResource()
             })
         }
+        // if the url doesn't contain a code show the login button.
         else {
             const div = document.getElementById("buttonDiv");
             div.textContent = '';
             const button = document.createElement("button");
             button.textContent = 'Login'
+            // clicking the login button redirects the user to main.js where we send a login request.
             button.addEventListener('click', () => window.location.href = `http://localhost:${env.VITE_PORT}`);
             div.appendChild(button);
         }
-        
+    
+    // if they are logged in...
     } else {
+        // show the logout button and send a resource access request.
         logoutButtonAndAccessResource()
     }
 })
 
+// We use this twice, so put it in a seperate function to minimize redundant code.
 const logoutButtonAndAccessResource = async () => {
+    // create a logout button that, when clicked, tells the client to log out (removes all tokens) and redirects the user to the this page to make sure they don't see the resource anymore.
     const div = document.getElementById("buttonDiv");
     div.textContent = '';
     const button = document.createElement("button");
@@ -44,6 +56,7 @@ const logoutButtonAndAccessResource = async () => {
     });
     div.appendChild(button);
 
+    // send a request to access a resource and show the html of that page.
     solidOidcClient.accessResource(`${env.VITE_RESOURCE_URI}`, 'GET').then(async (data) => {
         let text = await data.text();
         if (data.headers.get('content-type') === 'text/turtle'){
--- demo/demo-id-kit/main.js
+++ demo/demo-id-kit/main.js
@@ -3,10 +3,14 @@ import { SolidOidcClient, LocalStorageStore } from "@digita-ai/dgt-id-kit"
 
 const env = import.meta.env
 
+// clearing localStorage because we are reinitializing the store here anyway
 localStorage.removeItem('localStorageStore')
 
+// create the store
 const store = new LocalStorageStore();
 
+// create the client and initialize it
 const solidOidcClient = new SolidOidcClient(store, false, `${env.VITE_CLIENT_ID}`)
 
+// login to the issuer and redirect to our redirectUri
 solidOidcClient.loginWithIssuer(`http://localhost:${env.VITE_OIDC_PORT}`, 'openid', `http://${env.VITE_IP}:${env.VITE_PORT}/requests.html`)
