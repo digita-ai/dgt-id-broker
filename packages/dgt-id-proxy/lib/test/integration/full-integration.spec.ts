3aa74eebe4c9230f1fbc755901c3e0b2c662b7bd
JasperVandenberghen <jasper.vandenberghen@digita.ai>
2021-09-20 11:28:10 +0200

    test: added tests to check incoming parameters at client

--- packages/dgt-id-proxy/lib/test/integration/full-integration.spec.ts
+++ packages/dgt-id-proxy/lib/test/integration/full-integration.spec.ts
@@ -5,7 +5,8 @@ import { fromKeyLike, JWK, KeyLike } from 'jose/jwk/from_key_like';
 import { generateKeyPair } from 'jose/util/generate_key_pair';
 import { v4 as uuid } from 'uuid';
 import { SignJWT } from 'jose/jwt/sign';
-import { NodeHttpServer, NotFoundHttpError } from '@digita-ai/handlersjs-http';
+import { NodeHttpServer } from '@digita-ai/handlersjs-http';
+import { decode } from 'jose/util/base64url';
 import { variables, mainModulePath, configPath } from '../setup-tests';
 
 describe('full integration', () => {
@@ -56,7 +57,8 @@ describe('full integration', () => {
     registration_access_token: 'bsuodFwxgBWR3qE-pyxNeNbDhN1CWBs6oZuqkAooUgb',
   };
 
-  const fetchURL = 'http://localhost:3000';
+  const upstreamUrl = 'http://localhost:3000';
+  const proxyUrl = 'http://localhost:3003';
 
   const formDataForAccessToken = new URLSearchParams();
   formDataForAccessToken.set('grant_type', 'authorization_code');
@@ -75,7 +77,7 @@ describe('full integration', () => {
     'scope': 'mockScope',
     'client_id': 'mockClient',
     'iss': 'http://mock-issuer.com',
-    'aud': 'solid',
+    'aud': 'mockAud',
   };
 
   const header1 = {
@@ -88,7 +90,7 @@ describe('full integration', () => {
 
     fetchMock.enableMocks();
 
-    authUrl = 'http://localhost:3003/auth?response_type=code&code_challenge=I-k6SDaSeVEgkBaUbk6E4UxFAyV8Mb_3NNSHf9q6Gu8&code_challenge_method=S256&scope=openid&client_id=http%3A%2F%2Fclient.example.com%2Fclientapp%2Fprofile&redirect_uri=http%3A%2F%2Fclient.example.com%2Frequests.html';
+    authUrl = `${proxyUrl}/auth?response_type=code&code_challenge=I-k6SDaSeVEgkBaUbk6E4UxFAyV8Mb_3NNSHf9q6Gu8&code_challenge_method=S256&scope=openid&client_id=http%3A%2F%2Fclient.example.com%2Fclientapp%2Fprofile&redirect_uri=http%3A%2F%2Fclient.example.com%2Frequests.html`;
 
     const keyPair = await generateKeyPair('ES256');
     privateKey1 = keyPair.privateKey;
@@ -145,8 +147,9 @@ describe('full integration', () => {
 
     let params: URLSearchParams;
     let state: string;
+    let response: Response;
 
-    beforeEach(async () => {
+    beforeAll(async () => {
 
       // prevents the next fetch request from being mocked, resumes normal mocking behaviour after
       fetchMock.dontMockOnce();
@@ -161,7 +164,7 @@ describe('full integration', () => {
       );
 
       // the authentication request the proxy sends to the upstream server after passing it through all the necessary handlers
-      const clientAuthReq = nock(fetchURL, { encodedQueryParams: true })
+      const clientAuthReq = nock(upstreamUrl, { encodedQueryParams: true })
         .get(`/auth`)
         .query(true)
       // nock docs state you can't use arrow functions if you want to call this.req
@@ -173,6 +176,7 @@ describe('full integration', () => {
           params = new URLSearchParams(this.req.path.split('?')[1]);
           state = params.get('state');
 
+          // mock the upstream response from the auth endpoint
           return [ 302, 'found', { location: `http://localhost:3001/requests.html?code=DafduCFHR-wyUF2Y3uY_T9TCQwvJ_O8AD5z2c8ksglY&state=${state}` } ];
 
         });
@@ -181,69 +185,84 @@ describe('full integration', () => {
       const code = nock('http://localhost:3001')
         .get('/requests.html')
         .query(true)
-        .reply(200, 'succes');
+        .reply(200, 'success');
 
       // the initial auth request to the proxy
-      const response = await fetch(authUrl, {
+      response = await fetch(authUrl, {
         method: 'GET',
       });
 
     });
 
-    it('should still contain scope', async () => {
+    describe('checking auth parameters incoming at the upstream server after passing the proxy', () => {
 
-      expect(params.get('scope')).toBeTruthy();
+      it('should still contain the same scope', () => {
+
+        expect(params.get('scope')).toBeTruthy();
+        expect(params.get('scope')).toEqual('openid');
+
+      });
+
+      it('should still contain the same response_type', () => {
+
+        expect(params.get('response_type')).toBeTruthy();
+        expect(params.get('response_type')).toEqual('code');
+
+      });
+
+      it('should still contain client_id', () => {
+
+        expect(params.get('client_id')).toBeTruthy();
+
+      });
+
+      it('should replace the client-id with the one provided in the registration response', () => {
+
+        // check if the client-id is the one provided in the registration response instead of the one provided in the initial request
+        expect(params.get('client_id')).toEqual(mockRegisterResponse.client_id);
+
+      });
+
+      it('should still contain the same redirect_uri', () => {
+
+        expect(params.get('redirect_uri')).toBeTruthy();
+        expect(params.get('redirect_uri')).toEqual(redirect_uri);
+
+      });
+
+      it('should pass state to the endpoint in the params if not provided in the initial request', () => {
+
+        // check if state is present in the url params of the initial request
+        expect(state).toBeDefined();
+        // check if state is 36 digits (like uuids)
+        expect(state.length).toBe(36);
+
+      });
+
+      it('should not contain the code_challenge & code_challenge_method', () => {
+
+        expect(params.get('code_challenge')).toBeFalsy();
+        expect(params.get('code_challenge_method')).toBeFalsy();
+
+      });
 
     });
 
-    it('should still contain response_type', async () => {
+    describe('checking parameters coming from the upstream server outgoing to the client after passing the proxy', () => {
 
-      expect(params.get('response_type')).toBeTruthy();
+      it('should not contain state as a url parameter anymore if none was provided in the initial request', () => {
 
-    });
+        expect(response.url).toEqual('http://localhost:3001/requests.html?code=DafduCFHR-wyUF2Y3uY_T9TCQwvJ_O8AD5z2c8ksglY');
 
-    it('should still contain client_id', async () => {
+      });
 
-      expect(params.get('client_id')).toBeTruthy();
+      it('should contain a code ', () => {
 
-    });
+        const paramsAtClient = new URLSearchParams(response.url.split('?')[1]);
 
-    it('should still contain redirect_uri', async () => {
+        expect(paramsAtClient.get('code')).toBeTruthy();
 
-      expect(params.get('redirect_uri')).toBeTruthy();
-
-    });
-
-    it('should pass state to the endpoint in the params if not provided in the initial request', async () => {
-
-      // check if state is present in the url params of the initial request
-      expect(state).toBeDefined();
-      // check if state is 36 digits (like uuids)
-      expect(state.length).toBe(36);
-
-    });
-
-    // it('should pass state to the endpoint in the if provided in the initial request', async () => {
-
-    //   // add state to the intial request
-    //   const authUrlWithState = 'http://localhost:3003/auth?response_type=code&code_challenge=I-k6SDaSeVEgkBaUbk6E4UxFAyV8Mb_3NNSHf9q6Gu8&code_challenge_method=S256&scope=openid&client_id=http%3A%2F%2Fclient.example.com%2Fclientapp%2Fprofile&redirect_uri=http%3A%2F%2Fclient.example.com%2Frequests.html&state=123456';
-
-    //   // the initial auth request to the proxy
-    //   const response = await fetch(authUrlWithState, {
-    //     method: 'GET',
-    //   });
-
-    //   expect(state).toBeDefined();
-    //   // check thate state is still the same as in the initial request
-
-    //   expect(state).toEqual('123456');
-
-    // });
-
-    it('should replace the client-id with the one provided in the registration response', async () => {
-
-      // check if the client-id is the one provided in the registration response instead of the one provided in the initial request
-      expect(params.get('client_id')).toEqual(mockRegisterResponse.client_id);
+      });
 
     });
 
@@ -253,6 +272,7 @@ describe('full integration', () => {
 
     let reqBody: URLSearchParams;
     let initialRequest;
+    let response: Response;
 
     beforeEach(async () => {
 
@@ -268,7 +288,7 @@ describe('full integration', () => {
       });
 
       // mocks the proxy's token request to the upstream server
-      const token = nock(fetchURL)
+      const token = nock(upstreamUrl)
         .post('/token')
         .query(true)
         // docs said this does not work with arrow functions hence the line below
@@ -286,7 +306,7 @@ describe('full integration', () => {
       // prevents the next fetch request from being mocked, resumes normal mocking behavior after
       fetchMock.dontMockOnce();
 
-      // mocks the sequencal responses from the endpoints
+      // mocks the sequential responses from the endpoints
       fetchMock.mockResponses(
         [ clientRegistrationData, { headers: { 'content-type':'application/ld+json' }, status: 200 } ],
         [ JSON.stringify({ jwks_uri: 'http://pathtojwks.com' }), { status: 200 } ],
@@ -294,7 +314,7 @@ describe('full integration', () => {
       );
 
       // the initial token request to the proxy
-      const response = await fetch('http://localhost:3003/token', {
+      response = await fetch('http://localhost:3003/token', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
@@ -305,55 +325,222 @@ describe('full integration', () => {
 
     });
 
-    it('should contain client_id in the body parameters', async () => {
+    describe('checking token parameters incoming at the upstream server after passing the proxy', () => {
 
-      expect(reqBody.get('client_id')).toBeDefined();
+      it('should contain client_id in the body parameters', () => {
+
+        expect(reqBody.get('client_id')).toBeDefined();
+
+      });
+
+      it('should use the client-id from the registration response', () => {
+
+        // check that the client id is the one from the registration response
+        expect(reqBody.get('client_id')).toEqual(mockRegisterResponse.client_id);
+
+      });
+
+      it('should contain grant_type in the body parameters', () => {
+
+        expect(reqBody.get('grant_type')).toBeDefined();
+        expect(reqBody.get('grant_type')).toEqual('authorization_code');
+
+      });
+
+      it('should contain code in the body parameters', () => {
+
+        expect(reqBody.get('code')).toBeDefined();
+
+      });
+
+      it('should contain redirect_uri in the body parameters', () => {
+
+        expect(reqBody.get('redirect_uri')).toBeDefined();
+
+      });
+
+      it('should contain the same redirect_uri as the one provided to the auth request', () => {
+
+        // check that the redirect_uri is the same as the one provided in the initial request
+        expect(reqBody.get('redirect_uri')).toEqual(redirect_uri);
+
+      });
+
+      it('should have a recalculated content-length after switching the client id', () => {
+
+        // check that the content-length has been recalculated after switching the client id with the client id from the registration response
+        expect(initialRequest.headers['content-length']).toEqual(Buffer.byteLength(reqBody.toString(), initialRequest.headers['content-type']).toString());
+
+      });
+
+      it('should not contain the code verifier in the query parameters', () => {
+
+        // check that the code verifier is removed from the query parameters
+        expect(reqBody.get('code_verifier')).toBeFalsy();
+
+      });
+
+      it('should not contain a DPoP token header if one was provided in the initial request', () => {
+
+        expect(initialRequest.headers.dpop).toBeFalsy();
+
+      });
 
     });
 
-    it('should contain  grant_type in the body parameters', async () => {
+    describe('checking token response parameters coming from the upstream server going to the client after passing the proxy', () => {
 
-      expect(reqBody.get('grant_type')).toBeDefined();
+      let responseBodyJSON;
 
-    });
+      let access_token: string;
+      let decodedHeaderAccessToken;
+      let decodedPayloadAccessToken;
 
-    it('should contain code in the body parameters', async () => {
+      let id_token: string;
+      let decodedHeaderIdToken;
+      let decodedPayloadIdToken;
 
-      expect(reqBody.get('code')).toBeDefined();
+      beforeAll(async () => {
 
-    });
+        const res = new Response(response.body);
+        const blob = await res.blob();
+        responseBodyJSON = JSON.parse(await blob.text());
 
-    it('should contain redirect_uri in the body parameters', async () => {
+        access_token = responseBodyJSON.access_token;
+        decodedHeaderAccessToken = JSON.parse(decode(access_token.split('.')[0]).toString());
+        decodedPayloadAccessToken = JSON.parse(decode(access_token.split('.')[1]).toString());
 
-      expect(reqBody.get('redirect_uri')).toBeDefined();
+        id_token = responseBodyJSON.id_token;
+        decodedHeaderIdToken = JSON.parse(decode(id_token.split('.')[0]).toString());
+        decodedPayloadIdToken = JSON.parse(decode(id_token.split('.')[1]).toString());
 
-    });
+      });
 
-    it('should use the client-id from the registration response', async () => {
+      it('should have an access token with an expiration time of 2 hours', async () => {
 
-      // check that the client id is the one from the registration response
-      expect(reqBody.get('client_id')).toEqual(mockRegisterResponse.client_id);
+        expect(responseBodyJSON.expires_in).toEqual(7200);
 
-    });
+      });
 
-    it('should have a recalculated content-length after switching the client id', async () => {
+      it('should have a token_type of DPoP', () => {
 
-      // check that the content-length has been recalculated after switching the client id with the client id from the registration response
-      expect(initialRequest.headers['content-length']).toEqual(Buffer.byteLength(reqBody.toString(), initialRequest.headers['content-type']).toString());
+        expect(responseBodyJSON.token_type).toEqual('DPoP');
 
-    });
+      });
 
-    it('should not contain the code verifier in the query parameters', async () => {
+      it('should contain JWT tokens with valid lengths', async () => {
 
-      // check that the code verifier is removed from the query parameters
-      expect(reqBody.get('code_verifier')).toBeFalsy();
+        const splitAccessToken = access_token.split('.');
+        expect(splitAccessToken.length).toBeGreaterThan(2);
+        const splitIDToken = access_token.split('.');
+        expect(splitIDToken.length).toBeGreaterThan(2);
 
-    });
+      });
 
-    it('should contain the same redirect_uri as the one provided to the auth request', async () => {
+      describe('checking access token parameters', () => {
 
-      // check that the redirect_uri is the same as the one provided in the initial request
-      expect(reqBody.get('redirect_uri')).toEqual(redirect_uri);
+        it('should have a kid header that contains the correct kid', () => {
+
+          expect(decodedHeaderAccessToken.kid).toBeDefined();
+          expect(decodedHeaderAccessToken.kid).not.toEqual('mockKeyId');
+          expect(decodedHeaderAccessToken.kid).toEqual('Eqa03FG9Z7AUQx5iRvpwwnkjAdy-PwmUYKLQFIgSY5E');
+
+        });
+
+        it('should contain an alg header of ES256', () => {
+
+          expect(decodedHeaderAccessToken.alg).toBeDefined();
+          expect(decodedHeaderAccessToken.alg).toEqual('ES256');
+
+        });
+
+        it('should contain the correct client id', () => {
+
+          expect(decodedPayloadAccessToken.client_id).toEqual(client_id);
+
+        });
+
+        it('should contain a correct minted webid as webid claim', () => {
+
+          expect(decodedPayloadAccessToken.webid).toEqual(`http://localhost:3002/${payload1.sub}/profile/card#me`);
+
+        });
+
+        it('should contain a aud claim with solid audience', () => {
+
+          expect(decodedPayloadAccessToken.aud).toContain('solid');
+
+        });
+
+        it('should contain a correct jti claim and of the correct length', () => {
+
+          expect(decodedPayloadAccessToken.jti).toBeTruthy();
+          expect(decodedPayloadAccessToken.jti.length).toEqual(36); // correct length of a 4 UUID
+          expect(decodedPayloadAccessToken.jti).not.toEqual('mockJti');
+
+        });
+
+        it('should contain iss claim with a valid URL of the proxy', () => {
+
+          expect(decodedPayloadAccessToken.iss).toEqual(proxyUrl);
+
+        });
+
+        it('should contain iat and exp claims', () => {
+
+          expect(decodedPayloadAccessToken.iat).toBeDefined();
+          expect(decodedPayloadAccessToken.exp).toBeDefined();
+
+        });
+
+        it('should contain a cnf claim containing a jkt', () => {
+
+          expect(decodedPayloadAccessToken.cnf).toBeTruthy();
+          expect(decodedPayloadAccessToken.cnf.jkt).toBeTruthy();
+
+        });
+
+      });
+
+      describe('checking id_token parameters', () => {
+
+        it('should have a kid header that contains the correct kid', () => {
+
+          expect(decodedHeaderIdToken.kid).toBeDefined();
+          expect(decodedHeaderIdToken.kid).not.toEqual('mockKeyId');
+          expect(decodedHeaderIdToken.kid).toEqual('Eqa03FG9Z7AUQx5iRvpwwnkjAdy-PwmUYKLQFIgSY5E');
+
+        });
+
+        it('should contain a typ header of jwt', () => {
+
+          expect(decodedHeaderIdToken.typ).toBeDefined();
+          expect(decodedHeaderIdToken.typ).toEqual('JWT');
+
+        });
+
+        it('should contain an alg header of ES256', () => {
+
+          expect(decodedHeaderIdToken.alg).toBeDefined();
+          expect(decodedHeaderIdToken.alg).toEqual('ES256');
+
+        });
+
+        it('should contain a correct minted webid claim', () => {
+
+          expect(decodedPayloadIdToken.webid).toEqual(`http://localhost:3002/${payload1.sub}/profile/card#me`);
+
+        });
+
+        it('should contain a correct jti claim and of the correct length', () => {
+
+          expect(decodedPayloadIdToken.jti).toBeTruthy();
+          expect(decodedPayloadIdToken.jti.length).toEqual(36); // correct length of a 4 UUID
+          expect(decodedPayloadIdToken.jti).not.toEqual('mockJti');
+
+        });
+
+      });
 
     });
 
