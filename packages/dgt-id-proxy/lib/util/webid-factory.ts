22890e5c43f11c1729daf12c271ffff04075231e
JasperVandenberghen <jasper.vandenberghen@digita.ai>
2021-10-18 17:24:12 +0200

    chore: removed webid factory from util

--- packages/dgt-id-proxy/lib/util/webid-factory.ts
+++
@@ -1,7 +0,0 @@
-import { Observable } from 'rxjs';
-
-export interface WebIDFactory {
-  handle(input: { [x: string]: string }, intermediateOutput?: string): Observable<string>;
-  canHandle(input: { [x: string]: string }, intermediateOutput?: string): Observable<boolean>;
-}
-
