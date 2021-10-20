b01bdb58b61cce0083251720dfefa6edba92531d
JasperVandenberghen <jasper.vandenberghen@digita.ai>
2021-10-20 10:53:09 +0200

    feat: webid profile handler WIP

--- packages/dgt-id-proxy/lib/webid/webid-profile.handler.spec.ts
+++ packages/dgt-id-proxy/lib/webid/webid-profile.handler.spec.ts
@@ -1,24 +1,35 @@
+
+import fetchMock from 'jest-fetch-mock';
 import { WebIdProfileHandler } from './webid-profile.handler';
 
 describe('WebIdProfileHandler', () => {
 
-  const webId = 'http://solid.community.com/23121d3c-84df-44ac-b458-3d63a9a05497dollar/profile/card#me';
+  const webIdContent = `@prefix foaf: <http://xmlns.com/foaf/0.1/>.
+  @prefix solid: <http://www.w3.org/ns/solid/terms#>.
+  
+  <http://solid.community.com/23121d3c-84df-44ac-b458-3d63a9a05497dollar/profile/card#me> foaf:PersonalProfileDocument "";
+      foaf:maker "http://solid.community.com/23121d3c-84df-44ac-b458-3d63a9a05497dollar/profile/card#me";
+      foaf:primaryTopic "http://solid.community.com/23121d3c-84df-44ac-b458-3d63a9a05497dollar/profile/card#me";
+      foaf:given_name "Tony";
+      foaf:family_name "Paillard".`;
 
   const response = {
     body: {
       access_token: {
         header: {},
         payload: {
-          webId,
+          webId: 'http://solid.community.com/23121d3c-84df-44ac-b458-3d63a9a05497dollar/profile/card#me',
           'sub': '123456789',
         },
       },
       id_token: {
         header: {},
         payload: {
-          webId,
+          webId: 'http://solid.community.com/23121d3c-84df-44ac-b458-3d63a9a05497dollar/profile/card#me',
           'sub': '123456789',
           'username': '23121d3c-84df-44ac-b458-3d63a9a05497/|:$^?#{}[]',
+          'given_name': 'Tony',
+          'family_name': 'Paillard',
         },
       },
     },
@@ -26,11 +37,27 @@ describe('WebIdProfileHandler', () => {
     status: 200,
   };
 
-  const webIdProfileHandler = new WebIdProfileHandler();
+  const predicates = [
+    { tokenKey: 'http://xmlns.com/foaf/0.1/PersonalProfileDocument', predicate: '' },
+    { tokenKey: 'http://xmlns.com/foaf/0.1/maker', predicate: response.body.id_token.payload.webId },
+    { tokenKey: 'http://xmlns.com/foaf/0.1/primaryTopic', predicate: response.body.id_token.payload.webId },
+    { tokenKey: 'http://xmlns.com/foaf/0.1/given_name', predicate: response.body.id_token.payload.given_name },
+    { tokenKey: 'http://xmlns.com/foaf/0.1/family_name', predicate: response.body.id_token.payload.family_name },
+  ];
+
+  const webIdProfileHandler = new WebIdProfileHandler(predicates);
+
+  beforeAll(() => fetchMock.enableMocks());
 
   it('should be correctly instantiated', () => {
 
-    expect(new WebIdProfileHandler()).toBeTruthy();
+    expect(new WebIdProfileHandler(predicates)).toBeTruthy();
+
+  });
+
+  it('should error when no predicates are provided', () => {
+
+    expect(() => new WebIdProfileHandler(undefined)).toThrow('Predicate list is required');
 
   });
 
@@ -42,9 +69,36 @@ describe('WebIdProfileHandler', () => {
 
     });
 
-    fit('should', async () => {
+    it('should generate a body when profile document does not exist', async () => {
+
+      fetchMock.once(webIdContent, { headers: { 'content-type':'text/turtle' }, status: 404 });
+
+      webIdProfileHandler.generateProfileBody = jest.fn().mockReturnValue(webIdContent);
 
       await webIdProfileHandler.handle(response).toPromise();
+
+      expect(webIdProfileHandler.generateProfileBody).toHaveBeenCalledTimes(1);
+
+      expect(webIdProfileHandler.generateProfileBody)
+        .toHaveBeenCalledWith(predicates, response.body.id_token.payload.webId);
+
+      expect(webIdProfileHandler
+        .generateProfileBody(predicates, response.body.id_token.payload.webId)).toEqual(webIdContent);
+
+    });
+
+    it('should create a profile when document does not exist', async () => {
+
+      fetchMock.once(webIdContent, { headers: { 'content-type':'text/turtle' }, status: 404 });
+
+      webIdProfileHandler.createWebIdProfile = jest.fn();
+
+      await webIdProfileHandler.handle(response).toPromise();
+
+      expect(webIdProfileHandler.createWebIdProfile).toHaveBeenCalledTimes(1);
+
+      expect(webIdProfileHandler.createWebIdProfile)
+        .toHaveBeenCalledWith(response.body.id_token.payload.webId, webIdContent);
 
     });
 
--- packages/dgt-id-proxy/lib/webid/webid-profile.handler.ts
+++ packages/dgt-id-proxy/lib/webid/webid-profile.handler.ts
@@ -2,13 +2,16 @@ import { Handler } from '@digita-ai/handlersjs-core';
 import { Observable, of, throwError, from } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { HttpHandlerResponse } from '@digita-ai/handlersjs-http';
-import { getClientRegistrationData } from '../util/process-client-registration-data';
+import { Writer, DataFactory } from 'n3';
 
+const { literal, defaultGraph, quad } = DataFactory;
 export class WebIdProfileHandler extends Handler<HttpHandlerResponse, HttpHandlerResponse> {
 
-  constructor() {
+  constructor(private predicates: { tokenKey: string; predicate: string }[]) {
 
     super();
+
+    if (!predicates) { throw new Error('Predicate list is required'); }
 
   }
 
@@ -16,9 +19,22 @@ export class WebIdProfileHandler extends Handler<HttpHandlerResponse, HttpHandle
 
     if (!response) { return throwError(new Error('A response must be provided')); }
 
-    // this.getWebIdProfile(response.body.id_token.payload.webid);
+    return from(this.getWebIdProfile(response.body.id_token.payload.webId)).pipe(
+      switchMap((data) => {
 
-    return of(response);
+        if (data.status !== 200) {
+
+          this.createWebIdProfile(
+            response.body.id_token.payload.webId,
+            this.generateProfileBody(this.predicates, response.body.id_token.payload.webId)
+          );
+
+        }
+
+        return of(response);
+
+      })
+    );
 
   }
 
@@ -28,18 +44,54 @@ export class WebIdProfileHandler extends Handler<HttpHandlerResponse, HttpHandle
 
   }
 
-  //   getWebIdProfile = async (webid: string): Promise<Response>  => {
+  getWebIdProfile = async (webid: string): Promise<Response>  => {
 
-  //     const data = await fetch(webid, {
-  //       method: 'GET',
-  //       headers: {
-  //         Accept: 'application/ld+json',
-  //       },
-  //     });
+    const data = await fetch(webid, {
+      method: 'HEAD',
+      headers: {
+        Accept: 'text/turtle',
+      },
+    });
 
-  //     return data;
+    return data;
 
-  //   };
+  };
+
+  createWebIdProfile = async (webid: string, body: string): Promise<void>  => {
+
+    await fetch(webid, {
+      method: 'PUT',
+      headers: {
+        Accept: 'text/turtle',
+      },
+      body,
+    });
+
+  };
+
+  generateProfileBody = (predicates: { tokenKey: string; predicate: string }[], webid: string): string => {
+
+    const writer = new Writer({ prefixes: { foaf: 'http://xmlns.com/foaf/0.1/', solid: 'http://www.w3.org/ns/solid/terms#' } });
+    let body = '';
+
+    predicates.forEach((predicate) => {
+
+      writer.addQuad(
+        quad(
+          DataFactory.namedNode(webid),
+          DataFactory.namedNode(predicate.tokenKey),
+          literal(predicate.predicate),
+          defaultGraph()
+        )
+      );
+
+    });
+
+    writer.end((err, result) => body = result);
+
+    return body;
+
+  };
 
 }
 
