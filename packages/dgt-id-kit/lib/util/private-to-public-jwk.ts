96e92821c5571ed39cb66ee51585a175ddc50fc2
Wouter Termont <woutermont@gmail.com>
2021-10-21 13:30:53 +0200

    feat: key transform and json type utils (#252)
    * feat: key transform and json type utils
    
    Signed-off-by: Wouter Termont <woutermont@gmail.com>
    
    * build: update provider package locks
    
    Signed-off-by: Wouter Termont <woutermont@gmail.com>
    
    * fix: move jwk util to id-kit
    
    Signed-off-by: Wouter Termont <woutermont@gmail.com>
    
    * test: only possible test for new jwk util
    
    Signed-off-by: Wouter Termont <woutermont@gmail.com>

---
+++ packages/dgt-id-kit/lib/util/private-to-public-jwk.ts
@@ -0,0 +1,16 @@
+import { JWK } from 'jose/types';
+
+export const privateToPublicJwk = (jwk: JWK): JWK => ({
+  kty: jwk.kty, // key type: defines thecryptographic algorithm family used with the key
+  use: jwk.use, // The "use" parameter is employed to indicate whether a public key is used for encrypting data or verifying the signature on data.
+  // The "key_ops" (key operations) parameter identifies the operation(s) for which the key is intended to be used
+  key_ops: jwk.key_ops ? [ ...jwk.key_ops ] : undefined,
+  kid: jwk.kid, // The "kid" (key ID) parameter is used to match a specific key.
+  alg: jwk.alg, // The "key_ops" (key operations) parameter identifies the operation(s) for which the key is intended to be used
+  crv: jwk.crv, // if key type is elliptical curve, defines the specific curve (ES256, p-256, RS256, etc.)
+  e: jwk.e, // used by RSA key types
+  n: jwk.n, // used by RSA key types
+  x: jwk.x, // used by EC key types
+  x5c: jwk.x5c ? [ ...jwk.x5c ] : undefined, // optional parameter. Read more: https://tools.ietf.org/html/rfc7517#section-4.7
+  y: jwk.y, // used by EC key types
+});
