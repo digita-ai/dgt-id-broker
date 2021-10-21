a981f15844eaed6ec6ed99177e2cfb4242a35a18
Wouter Termont <woutermont@gmail.com>
2021-10-21 12:41:04 +0200

    test: only possible test for new jwk util
    Signed-off-by: Wouter Termont <woutermont@gmail.com>

---
+++ packages/dgt-id-kit/lib/util/private-to-public-jwk.spec.ts
@@ -0,0 +1,23 @@
+import { JWK } from 'jose/types';
+import { privateToPublicJwk } from './private-to-public-jwk';
+
+const privateJWK: JWK = {
+  'crv': 'P-256',
+  'x': 'ZXD5luOOClkYI-WieNfw7WGISxIPjH_PWrtvDZRZsf0',
+  'y': 'vshKz414TtqkkM7gNXKqawrszn44OTSR_j-JxP-BlWo',
+  'd': '07JS0yPt-fDABw_28JdENtlF0PTNMchYmfSXz7pRhVw',
+  'kty': 'EC',
+  'kid': 'Eqa03FG9Z7AUQx5iRvpwwnkjAdy-PwmUYKLQFIgSY5E',
+  'alg': 'ES256',
+  'use': 'sig',
+};
+
+describe('privateToPublicJwk()', () => {
+
+  it('should return the public fields of a given private JWK', async () => {
+
+    expect('d' in privateToPublicJwk(privateJWK)).toBeFalsy();
+
+  });
+
+});
