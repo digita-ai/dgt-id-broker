5d33c4e01b680648b9d708e5af3a2a7faf2062b8
Stijn Taelemans <stijn@digita.ai>
2021-10-26 13:20:20 +0200

    test: updated tests

--- packages/inrupt-solid-service/lib/solid-sdk.service.spec.ts
+++ packages/inrupt-solid-service/lib/solid-sdk.service.spec.ts
@@ -1,4 +1,4 @@
-import { addUrl, createSolidDataset, createThing, saveSolidDatasetAt, getDefaultSession } from '@digita-ai/inrupt-solid-client';
+import { addUrl, createSolidDataset, createThing, saveSolidDatasetAt, getDefaultSession, login } from '@digita-ai/inrupt-solid-client';
 import * as sdk from '@digita-ai/inrupt-solid-client';
 import { Issuer } from './models/issuer.model';
 import { SolidSDKService } from './solid-sdk.service';
@@ -54,6 +54,123 @@ describe('SolidSDKService', () => {
       expect(addUrlSpy).toHaveBeenCalledTimes(newIssuers.length);
       expect(setThingSpy).toHaveBeenCalledTimes(1);
       expect(sdk.saveSolidDatasetAt).toHaveBeenCalledTimes(1);
+
+    });
+
+  });
+
+  describe('login', () => {
+
+    const mockIssuer: Issuer = {
+      uri: 'https://issuer/',
+      icon: 'https://issuer/icon.png',
+      description: 'mock issuer',
+    };
+
+    const mockClient = {
+      [mockIssuer.uri]: {
+        clientName: 'test',
+        clientSecret: 'mockSecret',
+        clientId: 'mockId',
+      },
+    };
+
+    it('should error when webId is undefined', async () => {
+
+      service.getIssuers = jest.fn(async () => [ mockIssuer ]);
+      await expect(service.login(undefined)).rejects.toThrow('WebId should be set.: ');
+
+    });
+
+    it('should error when issuer could not be found', () => {
+
+      service.getIssuers = jest.fn(async () => undefined);
+      expect(service.login('https://web.id/')).rejects.toThrow('Issuer should be set.: ');
+
+    });
+
+    it('should call login with correct clientname', async () => {
+
+      (login as any) = jest.fn();
+      service.getIssuers = jest.fn(async () => [ mockIssuer ]);
+
+      await service.login('https://web.id/');
+
+      expect(sdk.login).toHaveBeenCalledWith(expect.objectContaining({
+        oidcIssuer: mockIssuer.uri,
+        clientName: mockClient[mockIssuer.uri].clientName,
+      }));
+
+    });
+
+    it('should call login with correct clientname, secret and id if set', async () => {
+
+      service = new SolidSDKService('test', mockClient);
+
+      (login as any) = jest.fn();
+      service.getIssuers = jest.fn(async () => [ mockIssuer ]);
+
+      await service.login('https://web.id/');
+
+      expect(sdk.login).toHaveBeenCalledWith(expect.objectContaining({
+        oidcIssuer: mockIssuer.uri,
+        clientName: mockClient[mockIssuer.uri].clientName,
+        clientId: mockClient[mockIssuer.uri].clientId,
+        clientSecret: mockClient[mockIssuer.uri].clientSecret,
+      }));
+
+    });
+
+  });
+
+  describe('loginWithIssuer', () => {
+
+    const mockIssuer: Issuer = {
+      uri: 'https://issuer/',
+      icon: 'https://issuer/icon.png',
+      description: 'mock issuer',
+    };
+
+    const mockClient = {
+      [mockIssuer.uri]: {
+        clientName: 'test',
+        clientSecret: 'mockSecret',
+        clientId: 'mockId',
+      },
+    };
+
+    it('should error when issuer is undefined', async () => {
+
+      await expect(service.loginWithIssuer(undefined)).rejects.toThrow('Issuer should be set.: ');
+
+    });
+
+    it('should call login with correct clientname', async () => {
+
+      (login as any) = jest.fn();
+
+      await service.loginWithIssuer(mockIssuer);
+
+      expect(sdk.login).toHaveBeenCalledWith(expect.objectContaining({
+        oidcIssuer: mockIssuer.uri,
+        clientName: mockClient[mockIssuer.uri].clientName,
+      }));
+
+    });
+
+    it('should call login with correct clientname, secret and id if set', async () => {
+
+      service = new SolidSDKService('test', mockClient);
+      (login as any) = jest.fn();
+
+      await service.loginWithIssuer(mockIssuer);
+
+      expect(sdk.login).toHaveBeenCalledWith(expect.objectContaining({
+        oidcIssuer: mockIssuer.uri,
+        clientName: mockClient[mockIssuer.uri].clientName,
+        clientId: mockClient[mockIssuer.uri].clientId,
+        clientSecret: mockClient[mockIssuer.uri].clientSecret,
+      }));
 
     });
 
