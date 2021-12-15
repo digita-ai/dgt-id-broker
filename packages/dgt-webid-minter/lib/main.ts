5725dd01230a0c392fc5b3e4c7fe2f9dbcf34ec9
Wouter Termont <woutermont@gmail.com>
2021-12-15 11:47:28 +0100

    chore: some setup changes
    Signed-off-by: Wouter Termont <woutermont@gmail.com>

--- packages/dgt-webid-minter/lib/main.ts
+++ packages/dgt-webid-minter/lib/main.ts
@@ -5,7 +5,7 @@ import { NodeHttpServer } from '@digita-ai/handlersjs-http';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
-export const checkUri = (uri: string) => {
+export const checkUri = (uri: string): { uri: string; host: string; port: string; scheme: string } => {
 
   const httpUri = uri.match(/^https?:\/\//g) ? uri : 'http://' + uri ;
 
@@ -36,11 +36,22 @@ export const checkUri = (uri: string) => {
  *
  * @param {Record<string, any>} variables - a record of values for the variables left open in the configuration.
  */
-export const launch: (variables: Record<string, any>) => Promise<void> = async (variables: Record<string, any>) => {
+export const launch = async (variables: Record<string, string>): Promise<void> => {
 
-  const mainModulePath = path.join(__dirname, '../');
+  const mainModulePathVariable = variables['urn:dgt-webid-minter:variables:mainModulePath'];
+  const customConfigPathVariable = variables['urn:dgt-webid-minter:variables:customConfigPath'];
 
-  const configPath = path.join(mainModulePath, 'config/minter-server-config.json');
+  const mainModulePath = mainModulePathVariable
+    ? path.isAbsolute(mainModulePathVariable)
+      ? mainModulePathVariable
+      : path.join(process.cwd(), mainModulePathVariable)
+    : path.join(__dirname, '../');
+
+  const configPath = customConfigPathVariable
+    ? path.isAbsolute(customConfigPathVariable)
+      ? customConfigPathVariable
+      : path.join(process.cwd(), customConfigPathVariable)
+    : path.join(__dirname, '../config/config-default.json');
 
   const manager = await ComponentsManager.build({
     mainModulePath,
@@ -54,16 +65,17 @@ export const launch: (variables: Record<string, any>) => Promise<void> = async (
   await server.start();
 
   // eslint-disable-next-line no-console -- top-level log
-  console.log(`WebID minter server started.`);
+  console.log(`WebID minter server started on ${variables['urn:dgt-webid-minter:variables:minterUri']}`);
 
 };
 
-export const createVariables = (args: string[]): Record<string, any> => {
+export const createVariables = (args: string[]): Record<string, string> => {
 
   const { argv: params } = yargs(hideBin(args))
     .usage('node ./dist/main.js [args]')
     .options({
       minterUri: { type: 'string', alias: 'u' },
+      config: { type: 'string', alias: 'c' },
       mainModulePath: { type: 'string', alias: 'm' },
     })
     .help();
@@ -76,8 +88,14 @@ export const createVariables = (args: string[]): Record<string, any> => {
       : path.join(process.cwd(), params.mainModulePath)
     : path.join(__dirname, '../');
 
+  const configPath = params.config
+    ? path.isAbsolute(params.config)
+      ? params.config
+      : path.join(process.cwd(), params.config)
+    : path.join(__dirname, '../config/config-default.json');
+
   return {
-    'urn:dgt-webid-minter:variables:customConfigPath': '../config/minter-server-config.json',
+    'urn:dgt-webid-minter:variables:customConfigPath': configPath,
     'urn:dgt-webid-minter:variables:mainModulePath': mainModulePath,
     'urn:dgt-webid-minter:variables:minterUri': minterUri,
     'urn:dgt-webid-minter:variables:minterHost': minterHost,
