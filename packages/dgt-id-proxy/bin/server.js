95620017ebd6eadd5a390d420d6d60256b17706a
LiamThys <liam.thys11@gmail.com>
2022-07-07 18:01:42 +0200

    chore: yargs in main.ts instead of server.js

--- packages/dgt-id-proxy/bin/server.js
+++ packages/dgt-id-proxy/bin/server.js
@@ -2,32 +2,6 @@
 
 const { createVariables, launch } = require('../dist/main.js');
 
-require('yargs/yargs')(process.argv.slice(2))
-  .options({
-    'proxyUri': {
-      alias: 'u',
-      describe: 'URL of the proxy',
-      demandOption: true
-    },
-    'upstreamUri': {
-      alias: 'U',
-      describe: 'URL of the upstream server',
-      demandOption: true
-    },
-    'openidConfigurationFilePath': {
-        alias: 'o',
-        describe: 'relative path to the OIDC configuration',
-        demandOption: true
-    },
-    'jwksFilePath': {
-        alias: 'j',
-        describe: 'relative path to the JWKs',
-        demandOption: true
-    }
-  })
-  .help()
-  .argv
-
 const vars = createVariables(process.argv);
 
 launch(vars);
