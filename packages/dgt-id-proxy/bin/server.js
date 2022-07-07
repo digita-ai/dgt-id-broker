a129354e62a9c56b3111e637e5f7a27fba0f6b06
LiamThys <liam.thys11@gmail.com>
2022-07-07 14:16:48 +0200

    feat: error messages proxy when run without args

--- packages/dgt-id-proxy/bin/server.js
+++ packages/dgt-id-proxy/bin/server.js
@@ -1,6 +1,17 @@
 #!/usr/bin/env node
 
+const commander = require('commander'); 
+
 const { createVariables, launch } = require('../dist/main.js');
+
+commander
+.requiredOption('-u, --proxyUri <uri>', 'URL of the proxy')
+.requiredOption('-U, --upstreamUri <uri>', 'URL of the upstream server')
+.requiredOption('-o, --openidConfigurationFilePath <path>', 'relative path to the OIDC configuration')
+.requiredOption('-j, --jwksFilePath <path>', 'elative path to the JWKs')
+.parse(process.argv); 
+
+console.log('Options: ', commander.opts());
 
 const vars = createVariables(process.argv);
 
